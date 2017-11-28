c124ce349ec6d2de0a923d94bed83dac319fc180
Suo Qin <kpsesuo@gmail.com>
2017-11-29 00:18:19 +1100

    create contract record from post handler

--- db/init.sql
+++ db/init.sql
@@ -1,2 +1,17 @@
 CREATE DATABASE IF NOT EXISTS zft;
 USE zft;
+
+create table if not exists contracts
+(
+	contractid bigint auto_increment,
+	hrid bigint default '0' not null,
+	uid bigint default '0' not null,
+	`from` bigint default '0' not null,
+	`to` bigint default '0' not null,
+	strategy text null,
+	expenses text null,
+	paytime varchar(3) not null,
+	signtime bigint default '0' not null,
+  primary key (`contractid`)
+) engine=innodb default charset=utf8;
+
--- services/v1.0/handlers/contracts.js
+++ services/v1.0/handlers/contracts.js
@@ -3,23 +3,46 @@
  * Operations on /contracts
  */
 module.exports = {
-    /**
-     * summary: save contract
-     * description: save contract information
+	/**
+	 * summary: save contract
+	 * description: save contract information
 
-     * parameters: body
-     * produces: application/json
-     * responses: 200, 400, 401, 406
-     */
-    post: function createContract(req, res, next) {
-        /**
-         * Get the data for response 200
-         * For response `default` status 200 is used.
-         */
-    },
-    //TODO: pure testing purpose, remove if necessary
-	get: function getContracts(req, res, next) {
-		res.send([])
-		return next()
+	 * parameters: body
+	 * produces: application/json
+	 * responses: 200, 400, 401, 406
+	 */
+	post: function createContract(req, res, next) {
+		/**
+		 * Get the data for response 200
+		 * For response `default` status 200 is used.
+		 */
+		const Contracts = MySQL.Contracts;
+		Contracts.sync().then(() => {
+			// Table created
+			Contracts.create({
+				hrid: 23,
+				uid: 223,
+				from: 1000,
+				to: 2000,
+				strategy: 'strategy',
+				expenses: 'expenses',
+				paytime: 'F03',
+				signtime: 3000
+			}).then(() => {
+				res.send(201, ErrorCode.ack(ErrorCode.OK, {}));
+				next();
+			}).catch((e) => {
+				console.log(e);
+				res.send(503, ErrorCode.ack(ErrorCode.DATABASEEXEC, e))
+				next();
+				}
+			);
+		});
+		console.log('init..');
 	},
+	//TODO: pure testing purpose, remove if necessary
+	get: function getContracts(req, res, next) {
+		res.send([]);
+		return next();
+	}
 };
