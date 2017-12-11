c15273e7b60d9ddfc92b6f7c56fd9a227121f1af
Louis Suo <louis.suo@myob.com>
2017-12-11 15:00:24 +1100

    use ES6 style of functions

--- services/proxy/proxy.js
+++ services/proxy/proxy.js
@@ -7,12 +7,12 @@ module.exports = {
 	delegate(path, res, next) {
 		const mockUrl = 'http://mock.doctorwork.com/mock/5a0e40653dea15470360bc0b/zft';
 		axios.get(`${mockUrl}/${path}`)
-			.then(function (response) {
+			.then((response) => {
 				console.log(response.data);
 				res.send(response.data);
 				next();
 			})
-			.catch(function (error) {
+			.catch((error) => {
 				console.log(error);
 			});
 	}
