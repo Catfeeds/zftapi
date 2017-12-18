6ceb3b12d16018a24eeaec63f59251063e8a7b7e
Louis Suo <louis.suo@myob.com>
2017-12-18 23:25:52 +0800

    generate regular bills

--- transformers/billGenerator.js
+++ transformers/billGenerator.js
@@ -3,20 +3,48 @@ const moment = require('moment');
 const _ = require('lodash');
 const fp = require('lodash/fp');
 
-const generateForContract = contract => [{
-	flow: 'receive',
-	entityType: 'property',
-	projectId: contract.projectId,
-	contractId: contract.id,
-	source: 'contract',
-	type: 'rent',
-	startDate: moment().subtract(2, 'month').unix(),
-	endDate: moment().subtract(1, 'month').unix(),
-	dueDate: moment().subtract(10, 'day').unix(),
-	createdAt: moment().unix(),
-	dueAmount: 12000,
-	metadata: {}
-}];
+const dueAt = (startDate, paymentPlan) => startDate;
+const expensesReduce = (expenses) =>  _.sumBy(_.filter(expenses, e => e.pattern === 'withRent'), 'rent');
+const dueAmountOf = (strategy, expenses) => strategy.freq.rent + expensesReduce(_.reject(expenses, e => e.configId === 111));
+const bondValue = (expenses) => _.find(expenses, e => e.configId === 111).rent;
+const oneTimeBills = (expenses, from, to) => _.filter(expenses, e => _.includes(['paidOff'], e.pattern));
+const regularBills = (expenses, from, to) => _.filter(expenses, e => _.includes(['1', '2', '3', '6', '12'], e.pattern));
+const generateForContract = contract => {
+	const from = contract.from;
+	const to = contract.to;
+	const paymentPlan = contract.paymentPlan;
+	const strategy = contract.strategy;
+	const expenses = contract.expenses;
+	const singleBill = (from, to) => ({
+		flow: 'receive',
+		entityType: 'property',
+		projectId: contract.projectId,
+		contractId: contract.id,
+		source: 'contract',
+		type: 'rent',
+		startDate: from,
+		endDate: to,
+		dueDate: dueAt(from, paymentPlan),
+		createdAt: moment().unix(),
+		dueAmount: dueAmountOf(strategy, expenses),
+		metadata: {}
+	});
+	const bondBill = (from, to) => ({
+		flow: 'receive',
+		entityType: 'property',
+		projectId: contract.projectId,
+		contractId: contract.id,
+		source: 'contract',
+		type: 'rent',
+		startDate: from,
+		endDate: to,
+		dueDate: dueAt(from, paymentPlan),
+		createdAt: moment().unix(),
+		dueAmount: bondValue(expenses),
+		metadata: {}
+	});
+	return _.concat([bondBill(from, to)], _.range(0, 12).map(m => singleBill(moment.unix(from).add(m, 'month').unix(), moment.unix(from).add(m + 1, 'month').unix())));
+};
 
 const extractBillItems = (contract, bill) => [{
 	billId: bill.id,
