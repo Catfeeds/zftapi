9817c547233dcfe0e8f42e3d0a3fd13326d9bab6
Louis Suo <louis.suo@myob.com>
2017-12-20 22:14:12 +0800

    add regular bills, paid off bills

--- test/transformers/billGenerator.spec.js
+++ test/transformers/billGenerator.spec.js
@@ -1,14 +1,12 @@
 'use strict';
-
+const moment = require('moment');
+const _ = require('lodash');
+const fp = require('lodash/fp');
 const generate = require('../../transformers/billGenerator').generateForContract;
 const removeNullValues = require('../../transformers/billGenerator').removeNullValues;
 
 describe('Bill generator', () => {
-	it('should generate bill base on contract', () => {
-		generate({id: 1}).should.have.lengthOf(1);
-	});
-
-	it('should omit null fields', () 	=> {
+	it('should omit null fields', () => {
 		removeNullValues({id: 1, dead: null, survive: 1, billItems: []})
 			.should.eql({id: 1, survive: 1, billItems: []});
 	});
@@ -16,12 +14,151 @@ describe('Bill generator', () => {
 	it('should omit null fields even in billItems array', () => {
 		const origin = {dead: null, survive: 1, keepMe: 0};
 		const after = {survive: 1, keepMe: 0};
-		removeNullValues({billItems: [ {dataValues: origin}, {dataValues: origin}]})
+		removeNullValues({billItems: [{dataValues: origin}, {dataValues: origin}]})
 			.should.eql({billItems: [after, after]});
 	});
 
 	it('should omit null fields only', () => {
 		removeNullValues({no: null, yes1: 1, yes2: 0, yes3: undefined, yes4: [], yes5: {}})
 			.should.eql({billItems: [], yes1: 1, yes2: 0, yes3: undefined, yes4: [], yes5: {}});
+	});
+	describe('generateForContract', function () {
+		it('should generate bills base on contract', () => {
+			const startDate = moment().unix();
+			const oneYearLater = moment().add(1, 'year').unix();
+			const bills = generate({
+				strategy: {
+					freq: {
+						rent: 3600,
+						pattern: '1'
+					}
+				},
+				expenses: [
+					{
+						configId: 111,
+						rent: 2600,
+						pattern: "paidOff"
+					}
+				],
+				"from": startDate,
+				"to": oneYearLater,
+				"paymentPlan": "-00",
+				projectId: 1,
+				contractId: 2,
+
+			});
+			bills.should.have.lengthOf(13);
+		});
+
+		it('should generate standard bills', () => {
+			const startDate = moment().unix();
+			const oneYearLater = moment().add(1, 'year').unix();
+			const bills = generate({
+				strategy: {
+					freq: {
+						rent: 3600,
+						pattern: '12'
+					}
+				},
+				expenses: [],
+				"from": startDate,
+				"to": oneYearLater,
+				"paymentPlan": "-00",
+				projectId: 1,
+				id: 2,
+
+			});
+			_.omit(bills[0], 'createdAt').should.be.eql({
+				flow: 'receive',
+				entityType: 'property',
+				projectId: 1,
+				contractId: 2,
+				source: 'contract',
+				type: 'rent',
+				startDate: startDate,
+				endDate: oneYearLater,
+				dueDate: startDate,
+				dueAmount: 3600,
+				metadata: {}
+			});
+		});
+
+		it('should generate one time paid off bills', () => {
+			const startDate = moment().unix();
+			const oneYearLater = moment().add(1, 'year').unix();
+			const bills = generate({
+				strategy: {
+					freq: {
+						rent: 3600,
+						pattern: '12'
+					}
+				},
+				expenses: [{
+					configId: 112,
+					rent: 45000,
+					pattern: 'paidOff'
+				}],
+				"from": startDate,
+				"to": oneYearLater,
+				"paymentPlan": "-00",
+				projectId: 1,
+				id: 2,
+
+			});
+			const paidOffBill = fp.filter(b => b.type === 'extra')(bills);
+			_.omit(paidOffBill[0], 'createdAt').should.be.eql({
+				flow: 'receive',
+				entityType: 'property',
+				projectId: 1,
+				contractId: 2,
+				source: 'contract',
+				type: 'extra',
+				startDate: startDate,
+				endDate: oneYearLater,
+				dueDate: startDate,
+				dueAmount: 45000,
+				metadata: {}
+			});
+		});
+		it('should generate regular bills', () => {
+			const startDate = moment().unix();
+			const oneYearLater = moment().add(1, 'year').unix();
+			const oneMonthLater = moment().add(1, 'month').unix();
+			const bills = generate({
+				strategy: {
+					freq: {
+						rent: 3600,
+						pattern: '12'
+					}
+				},
+				expenses: [{
+					configId: 112,
+					rent: 12000,
+					pattern: '1'
+				}],
+				"from": startDate,
+				"to": oneYearLater,
+				"paymentPlan": "-00",
+				projectId: 1,
+				id: 2,
+
+			});
+			console.log('bills', bills);
+			const regularBill = fp.filter(b => b.type === 'extra')(bills);
+			regularBill.should.have.length(12);
+			_.omit(regularBill[0], 'createdAt').should.be.eql({
+				flow: 'receive',
+				entityType: 'property',
+				projectId: 1,
+				contractId: 2,
+				source: 'contract',
+				type: 'extra',
+				startDate: startDate,
+				endDate: oneMonthLater,
+				dueDate: startDate,
+				dueAmount: 12000,
+				metadata: {}
+			});
+		});
 	});
 });
--- transformers/billGenerator.js
+++ transformers/billGenerator.js
@@ -4,18 +4,69 @@ const _ = require('lodash');
 const fp = require('lodash/fp');
 
 const dueAt = (startDate, paymentPlan) => startDate;
-const expensesReduce = (expenses) =>  _.sumBy(_.filter(expenses, e => e.pattern === 'withRent'), 'rent');
-const dueAmountOf = (strategy, expenses) => strategy.freq.rent + expensesReduce(_.reject(expenses, e => e.configId === 111));
-const bondValue = (expenses) => _.find(expenses, e => e.configId === 111).rent;
-const oneTimeBills = (expenses, from, to) => _.filter(expenses, e => _.includes(['paidOff'], e.pattern));
-const regularBills = (expenses, from, to) => _.filter(expenses, e => _.includes(['1', '2', '3', '6', '12'], e.pattern));
+
+const expensesReduce = (expenses) => _.sumBy(_.filter(expenses, e => e.pattern === 'withRent'), 'rent');
+
+const dueAmountOf = (strategy, expenses) => strategy.freq.rent + expensesReduce(expenses);
+
+const expenseAmount = (expense) => _.get(expense, 'rent');
+
+const billScheduler = (from, to, freq) => {
+	const monthDiff = Math.ceil(moment.duration(moment.unix(to).diff(moment.unix((from)))).asMonths())
+	const pace = _.parseInt(freq.pattern);
+	return _.range(0, monthDiff, pace)
+};
+
 const generateForContract = contract => {
 	const from = contract.from;
 	const to = contract.to;
 	const paymentPlan = contract.paymentPlan;
 	const strategy = contract.strategy;
 	const expenses = contract.expenses;
-	const singleBill = (from, to) => ({
+	const paidOffBills = (expenses, from, to) =>
+		fp.map(expense => paidOffBill(expense, from, to))
+		(fp.filter(e => _.includes(['paidOff'], e.pattern))(expenses));
+	const regularBills = (expenses, from, to) => _.flatten(
+		fp.map(expense => recursiveBill(expense, from, to))
+			(fp.filter(e => _.includes(['1', '2', '3', '6', '12'], e.pattern))(expenses)));
+
+	const paidOffBill = (expense, from, to) => ({
+		flow: 'receive',
+		entityType: 'property',
+		projectId: contract.projectId,
+		contractId: contract.id,
+		source: 'contract',
+		type: 'extra',
+		startDate: from,
+		endDate: to,
+		dueDate: dueAt(from, paymentPlan),
+		createdAt: moment().unix(),
+		dueAmount: expenseAmount(expense),
+		metadata: {}
+	});
+
+	const recursiveBill = (expense, from, to) => {
+		console.log(expense, from, to);
+		return billScheduler(from, to, expense.pattern).map(m =>
+			regularBill(expense, moment.unix(from).add(m, 'month').unix(),
+				moment.unix(from).add(m + _.parseInt(expense.pattern), 'month').unix()));
+	}
+
+	const regularBill = (expense, from, to) => ({
+		flow: 'receive',
+		entityType: 'property',
+		projectId: contract.projectId,
+		contractId: contract.id,
+		source: 'contract',
+		type: 'extra',
+		startDate: from,
+		endDate: to,
+		dueDate: dueAt(from, paymentPlan),
+		createdAt: moment().unix(),
+		dueAmount: expenseAmount(expense),
+		metadata: {}
+	});
+	const standardBill = (freq, from, to) => ({
 		flow: 'receive',
 		entityType: 'property',
 		projectId: contract.projectId,
@@ -29,22 +80,12 @@ const generateForContract = contract => {
 		dueAmount: dueAmountOf(strategy, expenses),
 		metadata: {}
 	});
-	const bondBill = (from, to) => ({
-		flow: 'receive',
-		entityType: 'property',
-		projectId: contract.projectId,
-		contractId: contract.id,
-		source: 'contract',
-		type: 'rent',
-		startDate: from,
-		endDate: to,
-		dueDate: dueAt(from, paymentPlan),
-		createdAt: moment().unix(),
-		dueAmount: bondValue(expenses),
-		metadata: {}
-	});
-	return _.concat([bondBill(from, to)], _.range(0, 12).map(m => singleBill(moment.unix(from).add(m, 'month').unix(), moment.unix(from).add(m + 1, 'month').unix())));
+	return _.concat(billScheduler(from, to, strategy.freq).map(m =>
+			standardBill(strategy.freq, moment.unix(from).add(m, 'month').unix(),
+				moment.unix(from).add(m + _.parseInt(strategy.freq.pattern), 'month').unix())),
+		paidOffBills(expenses, from, to), regularBills(expenses, from, to));
 };
+
 
 const extractBillItems = (contract, bill) => [{
 	billId: bill.id,
@@ -52,19 +93,19 @@ const extractBillItems = (contract, bill) => [{
 	configId: 123,
 	amount: 10000,
 	createdAt: moment().unix()
-},{
+}, {
 	billId: bill.id,
 	projectId: contract.projectId,
 	configId: 124,
 	amount: 11000,
 	createdAt: moment().unix()
-},{
+}, {
 	billId: bill.id,
 	projectId: contract.projectId,
 	configId: 125,
 	amount: 12000,
 	createdAt: moment().unix()
-},{
+}, {
 	billId: bill.id,
 	projectId: contract.projectId,
 	configId: 126,
