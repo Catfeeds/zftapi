f2d61480335dc6b4cde4c967a5011c1e5e3d5b88
Louis Suo <louis.suo@myob.com>
2017-12-26 23:46:51 +0800

    unit test of environments

--- test/setup.js
+++ test/setup.js
@@ -3,6 +3,8 @@ require("babel-core/register");
 require("babel-polyfill");
 
 const chai = require('chai');
-global.should = chai.should();
-const chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
+chai.should();
+
+chai.use(require('chai-as-promised'));
+chai.use(require('sinon-chai'));
+chai.use(require('chai-shallow-deep-equal'));
