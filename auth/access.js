212d1afdbd722add8d9c826d87579d5740a0f816
Louis Suo <louis.suo@myob.com>
2017-12-27 14:43:03 +0800

    unit tests for access

--- auth/access.js
+++ auth/access.js
@@ -1,7 +1,10 @@
 'use strict';
 const _ = require('lodash');
 
-const allowToCreateCredentials = (req) => req.isAuthenticated() && _.get(req, 'user.level', 'manager') === 'admin'
+const allowToCreateCredentials = (req) =>
+	!_.isUndefined(req.isAuthenticated)
+		&& req.isAuthenticated()
+		&& _.get(req, 'user.level', 'unknown') === 'admin';
 
 module.exports = {
 	allowToCreateCredentials
---
+++ test/auth/access.spec.js
@@ -0,0 +1,34 @@
+'use strict';
+import {allowToCreateCredentials} from '../../auth/access'
+
+describe('Access', function () {
+	it('should allow admin to create credentials', function () {
+		const req = {isAuthenticated: () => true, user: {level: 'admin'}};
+
+		allowToCreateCredentials(req).should.be.true;
+	});
+
+	it('should not allow other user to create credentials', function () {
+		const req = {isAuthenticated: () => true, user: {level: 'manager'}};
+
+		allowToCreateCredentials(req).should.not.be.true;
+	});
+
+	it('should not allow unknown user to create credentials', function () {
+		const req = {isAuthenticated: () => true};
+
+		allowToCreateCredentials(req).should.not.be.true;
+	});
+
+	it('should not allow unauthenticated user to create credentials', function () {
+		const req = {isAuthenticated: () => false, user: {level: 'admin'}};
+
+		allowToCreateCredentials(req).should.not.be.true;
+	});
+
+	it('should not allow anyone to create credentials by default', function () {
+		const req = {};
+
+		allowToCreateCredentials(req).should.not.be.true;
+	});
+});
