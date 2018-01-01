b219cfb2061e60708fc881f907be710a685d5b56
Louis Suo <louis.suo@myob.com>
2018-01-01 22:20:38 +0800

    unit test credentials post

--- services/v1.0/handlers/projects/:projectId/credentials.js
+++ services/v1.0/handlers/projects/:projectId/credentials.js
@@ -9,7 +9,6 @@ const _ = require('lodash');
 const access = require('../../../../../auth/access');
 
 const translate = (items) => {
-	console.log(`in translate ${items}`);
 	const innerValues = item => item.dataValues;
 	const omitNulls = item => _.omitBy(item, _.isNull);
 	const omitFields = item => _.omit(item, ['id', 'createdAt', 'updatedAt', 'password']);
@@ -17,7 +16,7 @@ const translate = (items) => {
 };
 
 module.exports = {
-	get: function getCredentials(req, res) {
+	get: async function getCredentials(req, res) {
 		const Auth = MySQL.Auth;
 		const projectId = req.params.projectId;
 		return Auth.findAll({
@@ -29,7 +28,7 @@ module.exports = {
 			.then(items => res.send(items))
 			.catch(err => res.send(500, ErrorCode.ack(ErrorCode.DATABASEEXEC, err)));
 	},
-	post: function createCredentials(req, res) {
+	post: async function createCredentials(req, res) {
 		const body = req.body;
 		const Auth = MySQL.Auth;
 
@@ -57,7 +56,7 @@ module.exports = {
 		if(!_.includes([Typedef.CredentialLevels.MANAGER, Typedef.CredentialLevels.ACCOUNTANT], level)) {
 			return res.send(403, ErrorCode.ack(ErrorCode.PERMISSIONDENIED, {error: "no allow to create admin level"}));
 		}
-
+		
 		return Auth.create({projectId, level, password, username})
 			.then(user =>
 				res.send(200, ErrorCode.ack(ErrorCode.OK, {username: user.username}))
--- test/services/credentials.spec.js
+++ test/services/credentials.spec.js
@@ -1,17 +1,20 @@
 'use strict';
 
-import {get} from '../../services/v1.0/handlers/projects/:projectId/credentials'
+import {get, post} from '../../services/v1.0/handlers/projects/:projectId/credentials'
 import 'include-node'
 import {spy, match} from 'sinon'
 
 describe('Environments', function () {
 	before(() => {
 		global.Typedef = Include('/libs/typedef');
+		global.ErrorCode = Include('/libs/errorCode');
 	});
 	it('should return all credentials from findAll', async function () {
-		const req = {params: {
-			projectId: 100
-		}};
+		const req = {
+			params: {
+				projectId: 100
+			}
+		};
 		global.MySQL = {
 			Auth: {
 				async findAll() {
@@ -27,9 +30,11 @@ describe('Environments', function () {
 	});
 
 	it('should omit id, createdAt, updatedAt, password fields', async function () {
-		const req = {params: {
-			projectId: 100
-		}};
+		const req = {
+			params: {
+				projectId: 100
+			}
+		};
 		global.MySQL = {
 			Auth: {
 				async findAll() {
@@ -45,9 +50,11 @@ describe('Environments', function () {
 	});
 
 	it('should omit null value fields', async function () {
-		const req = {params: {
-			projectId: 100
-		}};
+		const req = {
+			params: {
+				projectId: 100
+			}
+		};
 		global.MySQL = {
 			Auth: {
 				async findAll() {
@@ -60,6 +67,53 @@ describe('Environments', function () {
 		await get(req, {send: resSpy}).then(() =>
 			resSpy.should.have.been.calledWith([{onlyMe: 'haha'}])
 		)
+	});
+
+	it('should create auth if information are correct', async function () {
+		const req = {
+			isAuthenticated: () => true,
+			params: {
+				projectId: 100,
+			},
+			body: {
+				username: 'username',
+				level: 'MANAGER',
+				password: 'somepass'
+			},
+			user: {
+				level: 'ADMIN'
+			}
+		};
+		global.MySQL = {
+			Auth: {
+				async create(body) {
+					return body;
+				}
+			}
+		};
+
+		const resSpy = spy();
+
+		await post(req, {send: resSpy}).then(() =>
+			resSpy.should.have.been.calledWith(200, ErrorCode.ack(ErrorCode.OK, {username: req.body.username}))
+		)
+	});
+
+	it('should return 400 if password is empty', function () {
+		const req = {
+			params: {
+				projectId: 100,
+			},
+			body: {
+				username: 'username',
+				level: 'level'
+			}
+		};
+		const resSpy = spy();
+
+		post(req, {send: resSpy}).then(() =>
+			resSpy.should.have.been.calledWith(400, ErrorCode.ack(ErrorCode.PARAMETERERROR, {error: "please provide md5 encrypted password"}))
+		);
 	});
 
 });
