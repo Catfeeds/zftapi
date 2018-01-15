e5d31378f5366ffd29d23b680b97739c8bffbe13
Louis Suo <louis.suo@myob.com>
2018-01-15 20:00:49 +1100

    return all user fields in single contract

--- services/v1.0/handlers/projects/:projectId/contracts/:contractId.js
+++ services/v1.0/handlers/projects/:projectId/contracts/:contractId.js
@@ -18,7 +18,7 @@ module.exports = {
 		const Contracts = MySQL.Contracts;
 		const Users = MySQL.Users;
 		Contracts.findById(req.params.contractId, {
-			include: {model: Users, attributes: ['id', 'name', 'accountName']}
+			include: {model: Users}
 		})
 			.then(contract => {
 				if (fp.isEmpty(contract)) {
