41030755c6ce1ee433f68aca4a87bf4396ac1fd4
Louis Suo <louis.suo@myob.com>
2018-01-18 19:34:00 +1100

    remove stub payment data

--- services/v1.0/handlers/projects/:projectId/contracts/:contractId/bills.js
+++ services/v1.0/handlers/projects/:projectId/contracts/:contractId/bills.js
@@ -2,23 +2,11 @@
 /**
  * Operations on /contracts/{contractid}/bills
  */
-const _ = require('lodash');
+
 const fp = require('lodash/fp');
-const moment = require('moment');
 const removeNullValues = require('../../../../../../../transformers/billItemsCleaner').clearUpFields;
 
-const translate = bills => fp.map(
-	bill => fp.defaults({house: {houseId: 999}, paymentHistory: [
-		{amount: 10000, paymentChannel: 'alipay',
-			operator: 332, createdAt: moment().subtract(5, 'days').unix(), status: 'pending'},
-		{amount: 30000, paymentChannel: 'alipay',
-			operator: 332, createdAt: moment().subtract(9, 'days'), status: 'approved'},
-		{amount: 2000, paymentChannel: 'wechat',
-			operator: 331, createdAt: moment().subtract(7, 'days'), status: 'pending'},
-		{amount: 40000, paymentChannel: 'cash',
-			operator: 331, createdAt: moment().subtract(10, 'days'), status: 'declined'}
-	]})(removeNullValues(bill.dataValues))
-)(bills);
+const translate = bills => fp.map(bill => removeNullValues(bill.dataValues))(bills);
 
 module.exports = {
 	get: function getContractBills(req, res) {
