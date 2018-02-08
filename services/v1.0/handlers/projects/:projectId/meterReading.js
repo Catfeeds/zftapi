49e331363ef67df6671ee776dadd99c85eb2d9a9
Joey <mdolphin@126.com>
2018-02-08 22:56:36 +0800

    impl meterReading

--- services/v1.0/handlers/projects/:projectId/meterReading.js
+++ services/v1.0/handlers/projects/:projectId/meterReading.js
@@ -348,6 +348,101 @@ async function Gethouses(params, query) {
     }
 }
 
+const MAX_INT = 4294967295;
+function reGroupDetail(devices, contracts, devicePrices) {
+
+    const ONEDAY = 86400;
+    const belongTo = (dateA, dateB)=>{
+        return !(dateA[0] > dateB[1] || dateA[1] < dateB[0]);
+    };
+    const subtractDay = (date)=>{
+        return date - ONEDAY;
+    };
+    const matchItem = (dateBase, items)=>{
+        const length = items.length;
+        for(let i=0; i<length; i++){
+            const item = items[i];
+
+            const startDate = item.startDate || item.from;
+            const endDate = item.endDate || item.to || MAX_INT;
+
+            if(dateBase[0] > endDate || dateBase[1] < startDate){
+                continue;
+            }
+
+            if(dateBase[0] < startDate){
+                return {
+                    startDate: dateBase[0],
+                    endDate: subtractDay(startDate)
+                }
+            }
+
+            const date = minDate(dateBase, getDate(item));
+            return _.assign({
+                    startDate: date[0],
+                    endDate: date[1],
+                },
+                belongTo(date, getDate(item)) ? {item: item} : {}
+            );
+        }
+
+        return null;
+    };
+    const minDate = (dateA, dateB)=>{
+        const sortedDate = _.sortedUniq( _.concat([], dateA, dateB).sort() );
+
+        if(sortedDate.length < 2){
+            return null;
+        }
+
+        const value = dateA[0] || dateB[0];
+        const index = _.sortedIndexOf(sortedDate, value);
+        return [sortedDate[index], sortedDate[index+1]];
+    };
+    const getDate = (obj)=>{
+        if(!obj){
+            return [];
+        }
+        return [
+            obj.startDate || obj.from || 0,
+            obj.endDate || obj.to || MAX_INT
+        ];
+    };
+
+    let details = [];
+    _.each(devices, device=>{
+        const deviceDate = getDate(device);
+        const dateBase = getDate(device);
+        do {
+            const contract = matchItem(dateBase, contracts);
+            // console.info(debugLog(contract));
+
+            const housePrice = matchItem(dateBase, devicePrices);
+            // console.info(debugLog(housePrice));
+
+            const date = minDate(getDate(contract), getDate(housePrice));
+
+            console.info(date, device.deviceId, contract && contract.item.id, housePrice && housePrice.item.price);
+            details.push({
+                date: date,
+                device: device,
+                contract: contract,
+                price: housePrice
+            });
+            if(date) {
+                dateBase[0] = date[1];
+                dateBase[0] = Number( moment(dateBase[0], 'YYYYMMDD').add(1, 'days').format('YYYYMMDD') );
+            }
+
+            if(date[1] === deviceDate[1]){
+                break;
+            }
+        }while(1);
+    });
+
+    return details;
+}
+
 module.exports = {
     get: (req, res)=>{
         /**
@@ -361,8 +456,8 @@ module.exports = {
             const projectId = req.params.projectId;
             const deviceType = req.query.deviceType;
 
-            const timeFrom = req.query.timeFrom;
-            const timeTo = req.query.timeTo;
+            const timeFrom = req.query.startDate;
+            const timeTo = req.query.endDate;
 
             const houseFormat = req.query.houseFormat;
 
@@ -406,6 +501,31 @@ module.exports = {
                                 model: MySQL.Rooms
                                 , as: 'rooms'
                                 , required: true
+                                , include:[
+                                    {
+                                        model: MySQL.HouseDevices,
+                                        as: 'devices',
+                                        required: true,
+                                        where:{
+                                            startDate:{$gte: timeFrom},
+                                            endDate:{
+                                                $or:[
+                                                    {$lte: timeTo},
+                                                    {$eq: 0}
+                                                ]
+                                            },
+                                        }
+                                    },
+                                    {
+                                        model: MySQL.Contracts,
+                                        as: 'contracts',
+                                        required: false,
+                                        where:{
+                                            from: {$gte: timeFrom},
+                                            to: {$lte: timeTo}
+                                        }
+                                    }
+                                ]
                             },
                             {
                                 model: MySQL.Building, as: 'building'
@@ -424,41 +544,67 @@ module.exports = {
                                     endDate: 0,
                                     public: true
                                 }
+                            },
+                            {
+                                model: MySQL.HouseDevicePrice
+                                , as: 'prices'
+                                , required: true
+                                , where:{
+                                    category: 'CLIENT',
+                                    startDate:{$gte: timeFrom},
+                                    endDate:{
+                                        $or:[
+                                            {$lte: timeTo},
+                                            {$eq: 0}
+                                        ]
+                                    },
+                                }
                             }
                         ]
                     },
                     pagingInfo ? {offset: pagingInfo.skip, limit: pagingInfo.size} : {}
                     )
                 );
-
                 //
-                const roomIds = _.flatten(fp.map(house=>{
-                    return fp.map(room=>{return room.id;})(house.rooms);
-                })(houses));
-                const houseIds = fp.map(house=>{
-                    return house.id;
-                })(houses);
+                // //
+                // const roomIds = _.flatten(fp.map(house=>{
+                //     return fp.map(room=>{return room.id;})(house.rooms);
+                // })(houses));
+                // const houseIds = fp.map(house=>{
+                //     return house.id;
+                // })(houses);
+                //
+                // //seek contracts
+                // const contracts = await MySQL.Contracts.findAll({
+                //     where:{
+                //         roomId:{$in: roomIds},
+                //         from:{$gte: timeFrom},
+                //         to:{$lte: timeTo},
+                //     }
+                // });
+                // //seek houseDevicePrices
+                // const startDate = moment.unix(timeFrom).format('YYYYMMDD');
+                // const endDate = moment.unix(timeTo).format('YYYYMMDD');
+                // const houseDevicePrices = await MySQL.HouseDevicePrice.findAll({
+                //     where:{
+                //         projectId: projectId,
+                //         houseId: {$in: houseIds},
+                //         startDate:{$gte: startDate},
+                //         endDate:{$lte: endDate},
+                //     }
+                // });
 
-                //seek contracts
-                const contracts = await MySQL.Contracts.findAll({
-                    where:{
-                        roomId:{$in: roomIds},
-                        from:{$gte: timeFrom},
-                        to:{$lte: timeTo},
-                    }
-                });
-                //seek houseDevicePrices
-                const startDate = moment.unix(timeFrom).format('YYYYMMDD');
-                const endDate = moment.unix(timeTo).format('YYYYMMDD');
-                const houseDevicePrices = await MySQL.HouseDevicePrice.findAll({
-                    where:{
-                        projectId: projectId,
-                        houseId: {$in: houseIds},
-                        startDate:{$gte: startDate},
-                        endDate:{$lte: endDate},
-                    }
-                });
 
+                _.each(houses, house=>{
+                    const prices = house.prices;
+                    _.each(house.rooms, room=>{
+                        const devices = room.devices;
+                        const contracts = room.contracts;
+
+                        log.info(house.id, room.id, room.name);
+                        reGroupDetail(devices, contracts, prices);
+                    });
+                });
 
 
                 res.send(houses);
