978ba8a568920138b83bd6f58ce4cdfb0254b651
Louis Suo <louis.suo@myob.com>
2018-02-15 18:03:26 +1100

    refactor _ to fp

--- services/v1.0/handlers/projects/:projectId/users/:userId/contracts/contractId/bills/payment.js
+++ services/v1.0/handlers/projects/:projectId/users/:userId/contracts/contractId/bills/payment.js
@@ -1,16 +1,18 @@
 'use strict';
-const _ = require('lodash');
 const fp = require('lodash/fp');
 
 const common = Include('/services/v1.0/common');
 const assignNewId = common.assignNewId;
+
 /**
  * Operations on /fundChannels/{fundChannelId}
  */
 
-async function Pay(serviceCharge, projectId, fundChannel, contractId, bills, userId) {
+async function Pay(
+    serviceCharge, projectId, fundChannel, contractId, bills, userId) {
     const orderNo = assignNewId().id;
-    if(fundChannel.setting && fundChannel.setting.appid && fundChannel.setting.key){
+    if (fundChannel.setting && fundChannel.setting.appid &&
+        fundChannel.setting.key) {
         //online
 
         const metadata = {
@@ -19,22 +21,24 @@ async function Pay(serviceCharge, projectId, fundChannel, contractId, bills, use
             projectId: projectId,
             contractId: contractId,
             userId: userId,
-            billIds: fp.map(bill=>{
-                return bill.id;
-            })(bills)
+            billIds: fp.map('id')(bills),
         };
 
         try {
-            const result = await Util.charge(fundChannel, serviceCharge.amountForBill, orderNo, 'subject', 'body', metadata);
+            const result = await Util.charge(fundChannel,
+                serviceCharge.amountForBill, orderNo, 'subject', 'body',
+                metadata);
             return result;
         }
-        catch(e){
-            log.error(e, serviceCharge, projectId, fundChannel, contractId, bills, userId);
+        catch (e) {
+            log.error(e, serviceCharge, projectId, fundChannel, contractId,
+                bills, userId);
         }
     }
-    else{
+    else {
         //
-        const result = await common.payBills(MySQL)(bills, projectId, fundChannel, userId, orderNo);
+        const result = await common.payBills(MySQL)(bills, projectId,
+            fundChannel, userId, orderNo);
         return result;
     }
 }
@@ -48,7 +52,7 @@ module.exports = {
      * produces: application/json
      * responses: 200, 400
      */
-    patch: (req, res)=>{
+    patch: (req, res) => {
         /**
          * Get the data for response 200
          * For response `default` status 200 is used.
@@ -61,86 +65,99 @@ module.exports = {
         const billIds = body.billIds;
         const fundChannelId = body.fundChannelId;
 
-        if(!Util.ParameterCheck(body,
-            ['contractId', 'billIds', 'userId']
-        )){
+        if (!Util.ParameterCheck(body,
+            ['contractId', 'billIds', 'userId'],
+        )) {
             return res.send(422, ErrorCode.ack(ErrorCode.PARAMETERMISSED));
         }
 
-
-        (async()=>{
+        (async () => {
             try {
                 const receiveChannelAttributes = ['fee', 'setting', 'share'];
-                const fundChannelAttributes = ['category', 'flow', 'name', 'tag', 'id'];
+                const fundChannelAttributes = [
+                    'category',
+                    'flow',
+                    'name',
+                    'tag',
+                    'id'];
                 const result = await MySQL.ReceiveChannels.findOne({
-                    where:{
-                        fundChannelId: fundChannelId
+                    where: {
+                        fundChannelId: fundChannelId,
                     },
                     attributes: receiveChannelAttributes,
-                    include:[
+                    include: [
                         {
                             model: MySQL.FundChannels,
                             as: 'fundChannel',
-                            where:{
+                            where: {
                                 status: Typedef.FundChannelStatus.PASSED,
-                                projectId: projectId
+                                projectId: projectId,
                             },
                             attributes: fundChannelAttributes,
-                            include:[
+                            include: [
                                 {
                                     model: MySQL.ServiceCharge,
-                                    as: 'serviceCharge'
-                                }
-                            ]
-                        }
-                    ]
+                                    as: 'serviceCharge',
+                                },
+                            ],
+                        },
+                    ],
                 });
 
-                if(!result){
-                    return res.send(404, ErrorCode.ack(ErrorCode.CHANNELNOTEXISTS));
+                if (!result) {
+                    return res.send(404,
+                        ErrorCode.ack(ErrorCode.CHANNELNOTEXISTS));
                 }
 
-                const fundChannel = _.omit( _.assign(result.toJSON(), _.pick(result.fundChannel, _.concat(fundChannelAttributes, 'serviceCharge'))), 'fundChannel' );
+                const fundChannel = exports.moveFundChannelToRoot(result)(fundChannelAttributes);
                 const contract = await MySQL.Contracts.findOne({
                     where: {
                         id: contractId,
-                        projectId: projectId
+                        projectId: projectId,
                     },
                     include: [
                         {
                             model: MySQL.Bills,
                             as: 'bills',
                             where: {
-                                id: {$in: billIds}
-                            }
-                        }
-                    ]
+                                id: {$in: billIds},
+                            },
+                        },
+                    ],
                 });
                 if (!contract) {
-                    return res.send(404, ErrorCode.ack(ErrorCode.CONTRACTNOTEXISTS));
+                    return res.send(404,
+                        ErrorCode.ack(ErrorCode.CONTRACTNOTEXISTS));
                 }
 
                 if (contract.bills.length !== billIds.length) {
-                    return res.send(404, ErrorCode.ack(ErrorCode.BILLNOTEXISTS));
+                    return res.send(404,
+                        ErrorCode.ack(ErrorCode.BILLNOTEXISTS));
                 }
 
-                const amount = _.sum(fp.map(bill => {
-                    return bill.dueAmount;
-                })(contract.bills));
+                const amount = fp.sum(fp.map('dueAmount')(contract.bills));
 
                 const serviceCharge = common.serviceCharge(fundChannel, amount);
 
-                const payResult = await Pay(serviceCharge, projectId, fundChannel, contractId, contract.bills, userId);
-                if( payResult.code === ErrorCode.OK ){
+                const payResult = await Pay(serviceCharge, projectId,
+                    fundChannel, contractId, contract.bills, userId);
+                if (payResult.code === ErrorCode.OK) {
                     res.send();
                 }
-                else{
+                else {
                     res.send(500, payResult);
                 }
             }
-            catch(e){
+            catch (e) {
                 log.error(e, body);
             }
         })();
+    },
+    moveFundChannelToRoot: (result) => {
+        const requireServiceCharge= fp.concat('serviceCharge');
+        const fromFundChannel = fieldList => fp.pick(fieldList)(result.fundChannel);
+        const moveToRoot = fp.assign(result.toJSON());
+        const cleanUp = fp.omit('fundChannel');
+        return fp.pipe(requireServiceCharge, fromFundChannel, moveToRoot, cleanUp);
     }
 };
---
+++ test/services/users/contracts/bills/payments.spec.js
@@ -0,0 +1,18 @@
+const {moveFundChannelToRoot} = require(
+    '../../../../../services/v1.0/handlers/projects/:projectId/users/:userId/contracts/contractId/bills/payment');
+
+describe('payments', function() {
+    it('should pick serviceCharge and other attributes from result',
+        async function() {
+            moveFundChannelToRoot(
+                {
+                    toJSON: () => ({id: 99}),
+                    fundChannel: {aField: 1, serviceCharge: 2, notMe: 3},
+                },
+            )(['aField']).should.be.eql({id: 99, serviceCharge: 2, aField: 1});
+        });
+
+    it('should return empty if result is empty', async function() {
+        moveFundChannelToRoot({toJSON: () => ({})})([]).should.be.eql({});
+    });
+});
