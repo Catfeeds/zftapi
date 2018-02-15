ab5f78e9764c5a4572891f106b8cde7c3df59fef
Louis Suo <louis.suo@myob.com>
2018-02-15 19:21:26 +1100

    fix lint issue in price/type API, add unit tests

--- services/v1.0/handlers/projects/:projectId/prices/:type.js
+++ services/v1.0/handlers/projects/:projectId/prices/:type.js
@@ -1,103 +1,93 @@
 'use strict';
 
-const _ = require('lodash');
 const fp = require('lodash/fp');
 
 module.exports = {
-    put: (req, res) => {
+    put: async (req, res) => {
+        const projectId = req.params.projectId;
+        const type = req.params.type;
 
-        (async()=>{
-            const projectId = req.params.projectId;
-            const type = req.params.type;
+        const body = req.body;
 
-            const body = req.body;
+        if (!Util.ParameterCheck(body,
+                ['category', 'price'],
+            )) {
+            return res.send(422, ErrorCode.ack(ErrorCode.PARAMETERMISSED,
+                {error: 'please provide category & price in the body'}));
+        }
 
-            if(!Util.ParameterCheck(body,
-                ['category', 'price']
-            )){
-                return res.send(422, ErrorCode.ack(ErrorCode.PARAMETERMISSED, {error: 'missing query params houseFormat'}));
-            }
+        const getHouseIds = async () => {
 
-            const getHouseIds = async()=>{
+            const where = fp.assign({
+                projectId: projectId,
+                status: {$ne: Typedef.HouseStatus.DELETED},
+            })(body.houseIds ? {id: {$in: body.houseIds}} : {});
 
-                const where = _.assign(
-                    {
-                        projectId: projectId,
-                        status: {$ne: Typedef.HouseStatus.DELETED},
-                    },
-                    body.houseIds ? {id:{$in: body.houseIds}} : {}
-                );
+            const houses = await MySQL.Houses.findAll({
+                where: where,
+                attributes: ['id'],
+            });
 
-                const houses = await MySQL.Houses.findAll({
-                    where: where,
-                    attributes: ['id']
-                });
+            return fp.map('id')(houses);
+        };
 
-                return fp.map(house=>{return house.id;})(houses);
-            };
+        let t;
+        try {
+            const houseIds = await getHouseIds();
+            const devicePrices = await MySQL.HouseDevicePrice.findAll({
+                where: {
+                    sourceId: {$in: houseIds},
+                    projectId: projectId,
+                    type: type,
+                    category: body.category,
+                },
+            });
 
+            //
+            const updateIds = fp.map(price => {
+                return price.id;
+            })(devicePrices);
 
-            let t;
-            try {
-                const houseIds = await getHouseIds();
-                const devicePrices = await MySQL.HouseDevicePrice.findAll({
+            const updateHouseIds = fp.map(price => {
+                return price.sourceId;
+            })(devicePrices);
+            const createIds = fp.difference(houseIds, updateHouseIds);
+
+            const createPrices = fp.map(id => {
+                return {
+                    projectId: projectId,
+                    sourceId: id,
+                    type: type,
+                    category: body.category,
+                    price: body.price,
+                };
+            })(createIds);
+
+            t = await MySQL.Sequelize.transaction({autocommit: false});
+
+            await MySQL.HouseDevicePrice.update(
+                {
+                    price: body.price,
+                },
+                {
                     where: {
-                        sourceId: {$in: houseIds},
-                        projectId: projectId,
-                        type: type,
-                        category: body.category
-                    }
-                });
-
-                //
-                const updateIds = fp.map(price => {
-                    return price.id;
-                })(devicePrices);
-
-                const updateHouseIds = fp.map(price => {
-                    return price.sourceId;
-                })(devicePrices);
-                const createIds = _.difference(houseIds, updateHouseIds);
-
-                const createPrices = fp.map(id => {
-                    return {
-                        projectId: projectId,
-                        sourceId: id,
-                        type: type,
-                        category: body.category,
-                        price: body.price
-                    };
-                })(createIds);
-
-                t = await MySQL.Sequelize.transaction({autocommit: false});
-
-                await MySQL.HouseDevicePrice.update(
-                    {
-                        price: body.price
+                        id: updateIds,
                     },
-                    {
-                        where:{
-                            id: updateIds
-                        },
-                        transaction: t
-                    }
-                );
+                    transaction: t,
+                },
+            );
 
-                await MySQL.HouseDevicePrice.bulkCreate(createPrices, {transaction: t});
+            await MySQL.HouseDevicePrice.bulkCreate(createPrices,
+                {transaction: t});
 
-                await t.commit();
+            await t.commit();
 
-                res.send(204);
-            }
-            catch(e){
-                await t.rollback();
-                log.error(e, projectId, type, body);
-                res.send(500, ErrorCode.ack(ErrorCode.DATABASEEXEC));
-            }
-
-
-
-
-        })();
-    }
+            res.send(204);
+        }
+        catch (e) {
+            await t.rollback();
+            log.error(e, projectId, type, body);
+            res.send(500, ErrorCode.ack(ErrorCode.DATABASEEXEC));
+        }
+    },
 };
---
+++ test/services/prices/type.spec.js
@@ -0,0 +1,104 @@
+'use strict';
+
+const {put} = require(
+    '../../../services/v1.0/handlers/projects/:projectId/prices/:type');
+require('include-node');
+const {stub} = require('sinon');
+const fp = require('lodash/fp');
+
+describe('Prices', function() {
+    before(() => {
+        global.Typedef = Include('/libs/typedef');
+    });
+    it('should query houses by id if provided', async function() {
+        const req = {
+            params: {
+                projectId: 100,
+                type: '',
+            },
+            body: {
+                category: '',
+                price: '',
+                houseIds: [333, 222],
+            },
+        };
+        const sequelizeFindSpy = stub().resolves([]);
+
+        const Houses = {id: 'Houses', findAll: sequelizeFindSpy};
+
+        global.MySQL = {
+            Houses,
+            HouseDevicePrice: {
+                findAll: fp.noop,
+                update: fp.noop,
+                bulkCreate: fp.noop,
+            },
+            Sequelize: {
+                transaction: async () => ({
+                    commit: fp.noop,
+                    rollback: fp.noop,
+                }),
+            },
+        };
+
+        await put(req, {send: fp.noop}).then(() => {
+            sequelizeFindSpy.should.have.been.called;
+            const modelOptions = sequelizeFindSpy.getCall(0).args[0];
+            modelOptions.where.should.be.eql({
+                id: {
+                    $in: [
+                        333,
+                        222,
+                    ],
+                },
+                projectId: 100,
+                status: {
+                    $ne: 'DELETED',
+                },
+            });
+        });
+    });
+
+    it('should query houses with default condition if no id is provided',
+        async function() {
+            const req = {
+                params: {
+                    projectId: 100,
+                    type: '',
+                },
+                body: {
+                    category: '',
+                    price: '',
+                },
+            };
+            const sequelizeFindSpy = stub().resolves([]);
+
+            const Houses = {id: 'Houses', findAll: sequelizeFindSpy};
+
+            global.MySQL = {
+                Houses,
+                HouseDevicePrice: {
+                    findAll: fp.noop,
+                    update: fp.noop,
+                    bulkCreate: fp.noop,
+                },
+                Sequelize: {
+                    transaction: async () => ({
+                        commit: fp.noop,
+                        rollback: fp.noop,
+                    }),
+                },
+            };
+
+            await put(req, {send: fp.noop}).then(() => {
+                sequelizeFindSpy.should.have.been.called;
+                const modelOptions = sequelizeFindSpy.getCall(0).args[0];
+                modelOptions.where.should.be.eql({
+                    projectId: 100,
+                    status: {
+                        $ne: 'DELETED',
+                    },
+                });
+            });
+        });
+});
