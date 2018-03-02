1fcb0d960392fae328845452fea8e98d2e62fbcb
Joey <mdolphin@126.com>
2018-03-03 00:55:10 +0800

    support devices DELETE

--- services/v1.0/handlers/projects/:projectId/devices.js
+++ services/v1.0/handlers/projects/:projectId/devices.js
@@ -231,4 +231,43 @@ module.exports = {
 
         })();
     }
+    , delete: async(req, res)=>{
+
+        const projectId = req.params.projectId;
+
+        const deviceIds = req.body;
+
+        MySQL.HouseDevices.count({
+            where:{
+                projectId: projectId,
+                deviceId: {$in: deviceIds},
+                endDate: 0
+            }
+        }).then(
+            count=>{
+                if(count){
+                    return res.send(ErrorCode.ack(ErrorCode.DEVICEINBIND));
+                }
+
+                MySQL.Devices.destroy({
+                    where:{
+                        projectId: projectId,
+                        deviceId: {$in: deviceIds}
+                    }
+                }).then(
+                    ()=>{
+                        res.send(204);
+                    },
+                    err=>{
+                        log.error(err, projectId, deviceIds);
+                        res.send(ErrorCode.ack(ErrorCode.DATABASEEXEC));
+                    }
+                );
+            },
+            err=>{
+                log.error(err, projectId, deviceIds);
+                res.send(ErrorCode.ack(ErrorCode.DATABASEEXEC));
+            }
+        );
+    },
 };
