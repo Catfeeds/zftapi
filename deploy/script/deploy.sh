7c3ec075cd1db557c9732812c96ba71246b4fe5d
Joey <mdolphin@126.com>
2018-03-06 11:09:52 +0800

    fix environment variable TZ

--- deploy/script/deploy.sh
+++ deploy/script/deploy.sh
@@ -7,13 +7,13 @@ docker pull $REPO:$IMAGE_VERSION
 function start () {
   docker rm -f api
   docker rmi $(docker images -qf "before=$REPO:$IMAGE_VERSION" -f=reference="$REPO:v*")
-  docker run -e TZ="Asia/Shanghai" -e ENV -e REDIS_HOST -e REDIS_PORT -e REDIS_PASSWD -e ZFT_AMAP_KEY -e ZFT_EM_READ -e ZFT_EM_WRITE -d -p 8000:8000 \
+  docker run -e TZ -e ENV -e REDIS_HOST -e REDIS_PORT -e REDIS_PASSWD -e ZFT_AMAP_KEY -e ZFT_EM_READ -e ZFT_EM_WRITE -d -p 8000:8000 \
   	--name=api --net=zft  $REPO:$IMAGE_VERSION
 
 }
 function test_deploy() {
   docker rm -f test_api_deploy
-  docker run -e TZ="Asia/Shanghai" -e ENV -e REDIS_HOST -e REDIS_PORT -e REDIS_PASSWD -e AMAPKEY -e ZFT_AMAP_KEY -e ZFT_EM_READ -e ZFT_EM_WRITE -d \
+  docker run -e TZ -e ENV -e REDIS_HOST -e REDIS_PORT -e REDIS_PASSWD -e AMAPKEY -e ZFT_AMAP_KEY -e ZFT_EM_READ -e ZFT_EM_WRITE -d \
   	--name=test_api_deploy -p 8090:8000 $REPO:$IMAGE_VERSION
 }
 
