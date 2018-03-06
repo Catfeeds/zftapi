be42d4213e31892ee50b3db47c74ba57c404c8fa
Louis Suo <louis.suo@myob.com>
2018-03-06 21:18:25 +1100

    add housesInLocation parameter

--- services/v1.0/handlers/projects/:projectId/flows.js
+++ services/v1.0/handlers/projects/:projectId/flows.js
@@ -65,6 +65,173 @@ const translate = (models, pagingInfo) => {
 };
 
 const sqlDateTime = stamp => moment.unix(stamp).format('YYYY-MM-DD HH:mm:ss');
+
+const  groupByLocationId = (from, to, locationCondition) => {
+    return 'select\n' +
+        '  id, name,\n' +
+        '  sum(rentPart) as rent,\n' +
+        '  sum(rentPartFee) as rentFee,\n' +
+        '  sum(topupPart) as topup,\n' +
+        '  sum(topupFeePart) as topupFee,\n' +
+        '  sum(finalPayPart) as finalPay, \n' +
+        '  sum(finalReceivePart) as finalReceive, \n' +
+        '  (select sum(rentPart) - sum(rentPartFee) + sum(topupPart) - sum(topupFeePart) - sum(finalPayPart) + sum(finalReceivePart)) as balance ' +
+        ' from (select l.id, l.name,\n' +
+        '  sum(case\n' +
+        '      when f.category=\'rent\' then f.amount else 0\n' +
+        '      end) as rentPart,\n' +
+        '  sum(case\n' +
+        '      when f.category=\'rent\' then fee else 0\n' +
+        '      end) as rentPartFee,\n' +
+        '  0 as topupPart,\n' +
+        '  0 as topupFeePart,\n' +
+        '  sum(case\n' +
+        '      when (f.category=\'final\' and f.direction=\'pay\') then f.amount else 0\n' +
+        '      end) as finalPayPart, \n' +
+        '  sum(case\n' +
+        '      when (f.category=\'final\' and f.direction=\'receive\') then f.amount else 0\n' +
+        '      end) as finalReceivePart\n' +
+        'from\n' +
+        '  billpayment b,\n' +
+        '  bills b2,\n' +
+        '  flows f,\n' +
+        '  contracts c,\n' +
+        '  houses h,\n' +
+        '  rooms r,\n' +
+        '  location l,\n' +
+        '  buildings\n' +
+        'where\n' +
+        '  f.id = b.flowId\n' +
+        (from && to ?
+            '  and f.createdAt > :from  and f.createdAt < :to \n' :
+            '') +
+        '  and b2.id = b.billId\n' +
+        '  and c.id = b2.contractId\n' +
+        '  and b.projectId = :projectId \n' +
+        '  and c.roomId = r.id\n' +
+        '  and r.houseId = h.id\n' +
+        '  and h.buildingId = buildings.id\n' +
+        '  and buildings.locationId = l.id\n' +
+        locationCondition +
+        'GROUP BY l.id, l.name\n' +
+        ' UNION\n' +
+        'select l.id, l.name,\n' +
+        '  0 as rentPart,\n' +
+        '  0 as rentPartFee,\n' +
+        '  sum(case\n' +
+        '      when f.category=\'topup\' then f.amount else 0\n' +
+        '      end) as topupPart,\n' +
+        '  sum(case\n' +
+        '      when f.category=\'topup\' then fee else 0\n' +
+        '      end) as topupPartFee,\n' +
+        '  0 as finalPayPart, \n' +
+        '  0 as finalReceivePart \n' +
+        'from\n' +
+        '  topup t,\n' +
+        '  flows f,\n' +
+        '  contracts c,\n' +
+        '  houses h,\n' +
+        '  rooms r,\n' +
+        '  location l,\n' +
+        '  buildings\n' +
+        'where\n' +
+        '  f.id = t.flowId\n' +
+        (from && to ?
+            '  and f.createdAt > :from  and f.createdAt < :to \n' :
+            '') +
+        '  and c.id = t.contractId\n' +
+        '  and t.projectId = :projectId \n' +
+        '  and c.roomId = r.id\n' +
+        '  and r.houseId = h.id\n' +
+        '  and h.buildingId = buildings.id\n' +
+        '  and buildings.locationId = l.id\n' +
+        locationCondition +
+        'GROUP BY l.id, l.name\n' +
+        '     ) as f2\n' +
+        'GROUP BY id, name';
+};
+
+const  housesGroupByLocationId = (from, to, locationCondition) => {
+    return 'select\n' +
+        '  id, \n' +
+        '  sum(rentPart) as rent,\n' +
+        '  sum(rentPartFee) as rentFee,\n' +
+        '  sum(topupPart) as topup,\n' +
+        '  sum(topupFeePart) as topupFee,\n' +
+        '  sum(finalPayPart) as finalPay, \n' +
+        '  sum(finalReceivePart) as finalReceive, \n' +
+        '  (select sum(rentPart) - sum(rentPartFee) + sum(topupPart) - sum(topupFeePart) - sum(finalPayPart) + sum(finalReceivePart)) as balance ' +
+        ' from (select h.id,\n' +
+        '  sum(case\n' +
+        '      when f.category=\'rent\' then f.amount else 0\n' +
+        '      end) as rentPart,\n' +
+        '  sum(case\n' +
+        '      when f.category=\'rent\' then fee else 0\n' +
+        '      end) as rentPartFee,\n' +
+        '  0 as topupPart,\n' +
+        '  0 as topupFeePart,\n' +
+        '  sum(case\n' +
+        '      when (f.category=\'final\' and f.direction=\'pay\') then f.amount else 0\n' +
+        '      end) as finalPayPart, \n' +
+        '  sum(case\n' +
+        '      when (f.category=\'final\' and f.direction=\'receive\') then f.amount else 0\n' +
+        '      end) as finalReceivePart\n' +
+        'from\n' +
+        '  billpayment b,\n' +
+        '  bills b2,\n' +
+        '  flows f,\n' +
+        '  contracts c,\n' +
+        '  rooms r,\n' +
+        '  houses h,\n' +
+        '  buildings\n' +
+        ' where\n' +
+        '  f.id = b.flowId\n' +
+        (from && to ?
+            '  and f.createdAt > :from  and f.createdAt < :to \n' :
+            '') +
+        '  and b2.id = b.billId\n' +
+        '  and c.id = b2.contractId\n' +
+        '  and b.projectId = :projectId \n' +
+        '  and c.roomId = r.id\n' +
+        '  and r.houseId = h.id\n' +
+        '  and h.buildingId = buildings.id\n' +
+        locationCondition +
+        ' GROUP BY h.id\n' +
+        ' UNION\n' +
+        'select h.id,\n' +
+        '  0 as rentPart,\n' +
+        '  0 as rentPartFee,\n' +
+        '  sum(case\n' +
+        '      when f.category=\'topup\' then f.amount else 0\n' +
+        '      end) as topupPart,\n' +
+        '  sum(case\n' +
+        '      when f.category=\'topup\' then fee else 0\n' +
+        '      end) as topupPartFee,\n' +
+        '  0 as finalPayPart, \n' +
+        '  0 as finalReceivePart \n' +
+        'from\n' +
+        '  topup t,\n' +
+        '  flows f,\n' +
+        '  contracts c,\n' +
+        '  houses h,\n' +
+        '  rooms r,\n' +
+        '  buildings\n' +
+        ' where\n' +
+        '  f.id = t.flowId\n' +
+        (from && to ?
+            '  and f.createdAt > :from  and f.createdAt < :to \n' :
+            '') +
+        '  and c.id = t.contractId\n' +
+        '  and t.projectId = :projectId \n' +
+        '  and c.roomId = r.id\n' +
+        '  and r.houseId = h.id\n' +
+        '  and h.buildingId = buildings.id\n' +
+        locationCondition +
+        ' GROUP BY h.id\n' +
+        '     ) as f2\n' +
+        ' GROUP BY id';
+};
+
 module.exports = {
     get: async (req, res) => {
         const BillPayment = MySQL.BillPayment;
@@ -80,6 +247,7 @@ module.exports = {
         const query = req.query;
         const projectId = req.params.projectId;
         const locationIds = fp.get('locationIds')(query);
+        const housesInLocation = fp.get('housesInLocation')(query);
         const houseFormat = query.houseFormat;
         const from = query.from;
         const to = query.to;
@@ -92,84 +260,9 @@ module.exports = {
 
         const groupByCategory = async (req, res) => {
             const sequelize = MySQL.Sequelize;
-            const sql = 'select\n' +
-                '  id, name,\n' +
-                '  sum(rentPart) as rent,\n' +
-                '  sum(rentPartFee) as rentFee,\n' +
-                '  sum(topupPart) as topup,\n' +
-                '  sum(topupFeePart) as topupFee,\n' +
-                '  sum(finalPayPart) as finalPay, \n' +
-                '  sum(finalReceivePart) as finalReceive, \n' +
-                '  (select sum(rentPart) - sum(rentPartFee) + sum(topupPart) - sum(topupFeePart) - sum(finalPayPart) + sum(finalReceivePart)) as balance ' +
-                ' from (select l.id, l.name,\n' +
-                '  sum(case\n' +
-                '      when f.category=\'rent\' then f.amount else 0\n' +
-                '      end) as rentPart,\n' +
-                '  sum(case\n' +
-                '      when f.category=\'rent\' then fee else 0\n' +
-                '      end) as rentPartFee,\n' +
-                '  0 as topupPart,\n' +
-                '  0 as topupFeePart,\n' +
-                '  sum(case\n' +
-                '      when (f.category=\'final\' and f.direction=\'pay\') then f.amount else 0\n' +
-                '      end) as finalPayPart, \n' +
-                '  sum(case\n' +
-                '      when (f.category=\'final\' and f.direction=\'receive\') then f.amount else 0\n' +
-                '      end) as finalReceivePart\n' +
-                'from\n' +
-                '  billpayment b,\n' +
-                '  bills b2,\n' +
-                '  flows f,\n' +
-                '  contracts c,\n' +
-                '  houses h,\n' +
-                '  rooms r,\n' +
-                '  location l,\n' +
-                '  buildings\n' +
-                'where\n' +
-                '  f.id = b.flowId\n' +
-                (from && to ? '  and f.createdAt > :from  and f.createdAt < :to \n' : '') +
-                '  and b2.id = b.billId\n' +
-                '  and c.id = b2.contractId\n' +
-                '  and b.projectId = :projectId \n' +
-                '  and c.roomId = r.id\n' +
-                '  and r.houseId = h.id\n' +
-                '  and h.buildingId = buildings.id\n' +
-                '  and buildings.locationId = l.id\n' +
-                (locationIds ? '  and l.id in (:locationIds)' : '') +
-                'GROUP BY l.id, l.name\n' +
-                ' UNION\n' +
-                'select l.id, l.name,\n' +
-                '  0 as rentPart,\n' +
-                '  0 as rentPartFee,\n' +
-                '  sum(case\n' +
-                '      when f.category=\'topup\' then f.amount else 0\n' +
-                '      end) as topupPart,\n' +
-                '  sum(case\n' +
-                '      when f.category=\'topup\' then fee else 0\n' +
-                '      end) as topupPartFee,\n' +
-                '  0 as finalPayPart, \n' +
-                '  0 as finalReceivePart \n' +
-                'from\n' +
-                '  topup t,\n' +
-                '  flows f,\n' +
-                '  contracts c,\n' +
-                '  houses h,\n' +
-                '  rooms r,\n' +
-                '  location l,\n' +
-                '  buildings\n' +
-                'where\n' +
-                '  f.id = t.flowId\n' +
-                (from && to ? '  and f.createdAt > :from  and f.createdAt < :to \n' : '') +
-                '  and c.id = t.contractId\n' +
-                '  and t.projectId = :projectId \n' +
-                '  and c.roomId = r.id\n' +
-                '  and r.houseId = h.id\n' +
-                '  and h.buildingId = buildings.id\n' +
-                '  and buildings.locationId = l.id\n' +
-                (locationIds ? '  and l.id in (:locationIds)' : '') +
-                'GROUP BY l.id, l.name\n' +
-                '     ) as f2\n' +
-                'GROUP BY id, name';
+            const locationCondition = (locationIds ? '  and l.id in (:locationIds)' : '');
+            const sql = housesInLocation ? housesGroupByLocationId(from, to, ` and buildings.locationId = ${housesInLocation}`)
+                : groupByLocationId(from, to, locationCondition);
             const replacements = fp.defaults({projectId, locationIds})(
                 from && to ?
                     {
