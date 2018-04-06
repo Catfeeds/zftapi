558bafc877fc1df67c1974d1d31c81898da1001b
Louis Suo <louis.suo@myob.com>
2018-04-06 17:15:50 +1200

    allow to update user information

--- services/v1.0/handlers/projects/:projectId/users/:userId.js
+++ services/v1.0/handlers/projects/:projectId/users/:userId.js
@@ -11,6 +11,9 @@ const translate = model => {
 const resetPassword = model =>
     model.auth.updateAttributes({password: 'e10adc3949ba59abbe56e057f20f883e'});
 
+const updateFields = body => model =>
+    Promise.all([model.updateAttributes(body), model.auth.updateAttributes(body)]);
+
 module.exports = {
     get: async (req, res) => {
         const {projectId, userId} = req.params;
@@ -29,7 +32,7 @@ module.exports = {
             ],
         }).then(translate).then(user => res.send(user),
         ).catch(err => {
-            log.error(err, projectId, userId);
+            log.error('userId get', err, projectId, userId);
             res.send(500, ErrorCode.ack(ErrorCode.DATABASEEXEC));
         });
     },
@@ -44,8 +47,20 @@ module.exports = {
             include: [MySQL.Auth],
         }).then(resetPassword).then(() => res.send(ErrorCode.ack(ErrorCode.OK))
         ).catch(err => {
-            log.error(err, projectId, userId);
+            log.error('userId put',err, projectId, userId);
             res.send(500, ErrorCode.ack(ErrorCode.DATABASEEXEC));
         });
     },
+    patch: async (req, res) => {
+        const {projectId, userId} = req.params;
+
+        const fields = fp.pick(['mobile', 'documentId', 'documentType', 'gender', 'name'])(req.body);
+        return MySQL.Users.findById(userId, {
+            include: [MySQL.Auth],
+        }).then(updateFields(fields)).then(() => res.send(ErrorCode.ack(ErrorCode.OK))
+        ).catch(err => {
+            log.error('userId patch', err, projectId, userId);
+            res.send(500, ErrorCode.ack(ErrorCode.DATABASEEXEC));
+        });
+    }
 };
--- test/services/userId.spec.js
+++ test/services/userId.spec.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const fp = require('lodash/fp');
-const {get, put} = require(
+const {get, put, patch} = require(
     '../../services/v1.0/handlers/projects/:projectId/users/:userId');
 require('include-node');
 const {spy, stub} = require('sinon');
@@ -62,13 +62,43 @@ describe('Single user', function() {
                 },
             },
         };
-
-
-
         await put(req, {send: fp.noop}).then(() => {
             updateSpy.should.have.been.called;
             const response = updateSpy.getCall(0).args[0];
             response.should.be.eql({password: 'e10adc3949ba59abbe56e057f20f883e'});
+        });
+    });
+    it('should allow to patch user fields', async function() {
+        const authUpdateSpy = stub().resolves({});
+        const userUpdateSpy = stub().resolves({});
+        const userInfo = {
+            updateAttributes: userUpdateSpy,
+            auth: {
+                updateAttributes: authUpdateSpy
+            },
+        };
+        const req = {
+            params: {
+                projectId: 100,
+                userId: 123,
+            },
+            body: {
+                name: 'allow change me',
+                password: 'not allow to change me',
+            }
+        };
+        global.MySQL = {
+            Users: {
+                findById: async () => userInfo,
+            },
+        };
+        await patch(req, {send: fp.noop}).then(() => {
+            authUpdateSpy.should.have.been.called;
+            userUpdateSpy.should.have.been.called;
+            const authUpdateParams = authUpdateSpy.getCall(0).args[0];
+            authUpdateParams.should.be.eql(fp.omit('password')(req.body));
+            const userUpdateParams = userUpdateSpy.getCall(0).args[0];
+            userUpdateParams.should.be.eql(fp.omit('password')(req.body));
         });
     });
 });
