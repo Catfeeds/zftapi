4cd7bfd12f650ef8256dc214f6ac9594c36dd571
Louis Suo <louis.suo@myob.com>
2018-04-06 16:58:29 +1200

    add reset password api

--- services/v1.0/handlers/projects/:projectId/users/:userId.js
+++ services/v1.0/handlers/projects/:projectId/users/:userId.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const fp = require('lodash/fp');
+const {allowToResetPassword} = require('../../../../../../auth/access');
+
 const translate = model => {
     const user = model.toJSON();
     return fp.omit('auth')(fp.defaults(user.auth)(user));
 };
+
+const resetPassword = model =>
+    model.auth.updateAttributes({password: 'e10adc3949ba59abbe56e057f20f883e'});
 
 module.exports = {
     get: async (req, res) => {
@@ -27,5 +32,20 @@ module.exports = {
             log.error(err, projectId, userId);
             res.send(500, ErrorCode.ack(ErrorCode.DATABASEEXEC));
         });
-    }
+    },
+    put: async (req, res) => {
+        const {projectId, userId} = req.params;
+        if (!allowToResetPassword(req)) {
+            return res.send(403, ErrorCode.ack(ErrorCode.NOTSUPPORT,
+                {error: 'Only admin and manager can reset password.'}));
+        }
+
+        return MySQL.Users.findById(userId, {
+            include: [MySQL.Auth],
+        }).then(resetPassword).then(() => res.send(ErrorCode.ack(ErrorCode.OK))
+        ).catch(err => {
+            log.error(err, projectId, userId);
+            res.send(500, ErrorCode.ack(ErrorCode.DATABASEEXEC));
+        });
+    },
 };
--- test/services/userId.spec.js
+++ test/services/userId.spec.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const {get} = require(
+const fp = require('lodash/fp');
+const {get, put} = require(
     '../../services/v1.0/handlers/projects/:projectId/users/:userId');
 require('include-node');
-const {spy} = require('sinon');
+const {spy, stub} = require('sinon');
+
 
 describe('Single user', function() {
     it('should return user information', async function() {
@@ -34,6 +36,39 @@ describe('Single user', function() {
         await get(req, {send: resSpy}).then(() => {
             const response = resSpy.getCall(0).args[0];
             response.should.be.eql({name: 'name', username: 'username'});
+        });
+    });
+    it('should reset password by put method', async function() {
+        const updateSpy = stub().resolves({});
+        const userInfo = {
+            auth: {
+                updateAttributes: updateSpy
+            },
+        };
+        const req = {
+            isAuthenticated: () => true,
+            user: {
+                level: 'ADMIN',
+            },
+            params: {
+                projectId: 100,
+                userId: 123,
+            },
+        };
+        global.MySQL = {
+            Users: {
+                async findById() {
+                    return userInfo;
+                },
+            },
+        };
+
+
+
+        await put(req, {send: fp.noop}).then(() => {
+            updateSpy.should.have.been.called;
+            const response = updateSpy.getCall(0).args[0];
+            response.should.be.eql({password: 'e10adc3949ba59abbe56e057f20f883e'});
         });
     });
 });
