7bd124976599b3857cb544d586d847f41d1f4cde
Louis Suo <louis.suo@myob.com>
2018-04-08 22:57:59 +1000

    add build.sh

---
+++ build.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+function functional-tests {
+    docker-compose down && docker-compose -f docker-compose-test.yml run test
+}
+
+
+function usage {
+  echo Usage:
+  echo ======================
+  echo l for eslint
+  echo u for unit tests
+  echo f for functional tests
+  echo a for both above
+  echo none of the above will trigger 'npm t' only
+  echo ======================
+}
+
+
+function main {
+  	case $1 in
+		l) npm run lint ;;
+		u) npm t ;;
+		f) functional-tests ;;
+		a) npm t && functional-tests ;;
+		h) usage ;;
+		*) usage ;;
+	esac
+}
+
+main $@
