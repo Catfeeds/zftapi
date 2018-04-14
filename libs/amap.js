e739b7f136054fababf94f063caa99ceeb054d20
Louis Suo <louis.suo@myob.com>
2018-04-14 18:39:07 +1000

    fix lint issue in amap.js

--- libs/amap.js
+++ libs/amap.js
@@ -1,21 +1,18 @@
-const _ = require('lodash');
+const fp = require('lodash/fp');
 const got = require('got');
 const config = require('config');
 
-
 const URL = 'http://restapi.amap.com/v3/assistant/';
 
-function ParameterToQuery(param) {
-    let values = [];
-    _.mapKeys(param, (v, k)=>{
-        values.push(`${k}=${encodeURIComponent(v)}`);
-    });
-    return values.toString().replace(/,/g, '&');
-}
+const toURLQuery = fp.pipe(fp.entries,
+    fp.map(([k, v]) => `${k}=${encodeURIComponent(v)}`),
+    fp.join('&'));
 
-exports.InputTips = (query)=>{
-    return new Promise((resolve, reject)=>{
-        const reqStr = ParameterToQuery(query);
+exports.toURLQuery = toURLQuery;
+
+exports.InputTips = (query) => {
+    return new Promise((resolve, reject) => {
+        const reqStr = toURLQuery(query);
         const reqURL = `${URL}inputtips?output=json&key=${config.MAP}&${reqStr}&type=120304|120303|120302|120301|120300|120203|120202|120201|120200|120100|120000|100201|100200|100105|100104|100103|100102|100100|100000`;
 
         (async () => {
