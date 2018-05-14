5f6f02a1e2abf5926f4da8be44cb8271a2b4b54e
Louis Suo <louis.suo@myob.com>
2018-05-14 22:17:45 +1000

    refacter test code

--- test/services/devices/reading.spec.js
+++ test/services/devices/reading.spec.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const fp = require('lodash/fp');
 const moment = require('moment');
 const {spy} = require('sinon');
 const {get} = require(
@@ -9,86 +8,25 @@ const {get} = require(
 describe('Reading meters', function() {
     before(() => {
         global.Typedef = Include('/libs/typedef');
-        global.ErrorCode = Include('/libs/errorCode');
-        global.Util = Include('/libs/util');
-        global.SnowFlake = {next: fp.constant(1)};
     });
-    afterEach(() => {
-    });
-    it('should return rooms with detail', async function() {
-        const house = {
-            toJSON: () => ({
-                building: {
-                    building: 'building',
-                    unit: 'unit',
-                    location: 'location',
-                },
-                roomNumber: 'roomNumber',
-                prices: [
-                    {
-                        price: 1000,
-                    },
-                ],
-                rooms: [
-                    {
-                        id: 'roomId',
-                        name: 'roomName',
-                        contracts: [{
-                            userId: 222,
-                            user: {
-                                name: 'username',
-                            },
-                            from: 200000,
-                            to: 300000,
-                        }],
-                        devices: [
-                            {
-                                deviceId: 123,
-                            },
-                        ],
-                    },
-                ],
-            }),
-        };
-        const req = {
-            params: {
-                projectId: 100,
-            },
-            query: {
-                houseFormat: 'SHARE',
-                startDate: 100000,
-                endDate: 200000,
-            },
 
-        };
-        global.MySQL = {
-            Houses: {
-                async findAll() {
-                    return [house];
-                },
-            },
-            DeviceHeartbeats: {
-                async findAll() {
-                    return [{toJSON: () => ({deviceId: 123, startScale: 100, endScale: 200, startDate: 100, endDate: 200})}];
-                },
-            },
-            Sequelize: {
-                fn: () => {},
-                col: () => {},
-            }
-        };
+    it('should return rooms with detail', async function() {
+        const houses = [
+            {
+                toJSON: () => ({
+                    ...houseFields(),
+                    rooms: standardRooms(),
+                }),
+            }];
+        const heartbeats = [standardHeartbeat()];
+        global.MySQL = stubSequelizeModel(houses, heartbeats);
         const resSpy = spy();
 
-        await get(req, {send: resSpy}).then(() => {
+        await get(standardRequest(), {send: resSpy}).then(() => {
             resSpy.should.have.been.called;
             resSpy.getCall(0).args[0].data.should.be.eql([
                 {
-                    location: 'location',
-                    roomId: 'roomId',
-                    roomName: 'roomName',
-                    roomNumber: 'roomNumber',
-                    unit: 'unit',
-                    building: 'building',
+                    ...expectedFields,
                     details: [
                         {
                             amount: 1000 * 100,
@@ -108,83 +46,23 @@ describe('Reading meters', function() {
                 }]);
         });
     });
-    it('should ignore duplicated devices in one room', async function() {
-        const house = {
-            toJSON: () => ({
-                building: {
-                    building: 'building',
-                    unit: 'unit',
-                    location: 'location',
-                },
-                roomNumber: 'roomNumber',
-                prices: [
-                    {
-                        price: 1000,
-                    },
-                ],
-                rooms: [
-                    {
-                        id: 'roomId',
-                        name: 'roomName',
-                        contracts: [{
-                            userId: 222,
-                            user: {
-                                name: 'username',
-                            },
-                            from: 200000,
-                            to: 300000,
-                        }],
-                        devices: [
-                            {
-                                deviceId: 123,
-                            },
-                            {
-                                deviceId: 123,
-                            },
-                        ],
-                    },
-                ],
-            }),
-        };
-        const req = {
-            params: {
-                projectId: 100,
-            },
-            query: {
-                houseFormat: 'SHARE',
-                startDate: 100000,
-                endDate: 200000,
-            },
-
-        };
-        global.MySQL = {
-            Houses: {
-                async findAll() {
-                    return [house];
-                },
-            },
-            DeviceHeartbeats: {
-                async findAll() {
-                    return [{toJSON: () => ({deviceId: 123, startScale: 100, endScale: 200, startDate: 100, endDate: 200})}];
-                },
-            },
-            Sequelize: {
-                fn: () => {},
-                col: () => {},
-            }
-        };
+    it('should ignore duplicated devices in one room', async () => {
+        const houses = [
+            {
+                toJSON: () => ({
+                    ...houseFields(),
+                    rooms: standardRooms(),
+                }),
+            }];
+        const heartbeats = [standardHeartbeat()];
+        global.MySQL = stubSequelizeModel(houses, heartbeats);
         const resSpy = spy();
 
-        await get(req, {send: resSpy}).then(() => {
+        await get(standardRequest(), {send: resSpy}).then(() => {
             resSpy.should.have.been.called;
             resSpy.getCall(0).args[0].data.should.be.eql([
                 {
-                    location: 'location',
-                    roomId: 'roomId',
-                    roomName: 'roomName',
-                    roomNumber: 'roomNumber',
-                    unit: 'unit',
-                    building: 'building',
+                    ...expectedFields,
                     details: [
                         {
                             amount: 1000 * 100,
@@ -205,67 +83,34 @@ describe('Reading meters', function() {
         });
     });
     it('should return house and its rooms with detail', async function() {
-        const house = {
-            toJSON: () => ({
-                id: 199,
-                building: {
-                    building: 'building',
-                    unit: 'unit',
-                    location: 'location',
-                },
-                roomNumber: 'roomNumber',
-                prices: [
-                    {
-                        price: 2000,
-                    },
-                ],
-                devices: [
-                    {
-                        deviceId: 234,
-                        deviceHeartbeats: [
-                            {
-                                total: 900,
-                            },
-                            {
-                                total: 2000,
-                            },
-                        ],
-                    },
-                ],
-            }),
-        };
-        const req = {
-            params: {
-                projectId: 100,
-            },
-            query: {
-                houseFormat: 'SHARE',
-                startDate: 100000,
-                endDate: 200000,
-                houseId: 199,
-            },
-
-        };
-        global.MySQL = {
-            Houses: {
-                async findAll() {
-                    return [house];
-                },
-            },
-            DeviceHeartbeats: {
-                async findAll() {
-                    return [{toJSON: () => ({deviceId: 123, startScale: 100, endScale: 200, startDate: 100, endDate: 200})},
-                        {toJSON: () => ({deviceId: 234, startScale: 900, endScale: 2000, startDate: 100, endDate: 200})}];
-                },
-            },
-            Sequelize: {
-                fn: () => {},
-                col: () => {},
-            }
-        };
+        const houses = [
+            {
+                toJSON: () => ({
+                    id: 199,
+                    ...houseFields(),
+                    prices: [
+                        {
+                            price: 2000,
+                        },
+                    ],
+                    devices: [
+                        {
+                            deviceId: 234,
+                        },
+                    ],
+                }),
+            }];
+        const heartbeats = [
+            standardHeartbeat(),
+            standardHeartbeat({
+                deviceId: 234,
+                startScale: 900,
+                endScale: 2000,
+            })];
+        global.MySQL = stubSequelizeModel(houses, heartbeats);
         const resSpy = spy();
 
-        await get(req, {send: resSpy}).then(() => {
+        await get(standardRequest({houseId: 199}), {send: resSpy}).then(() => {
             resSpy.should.have.been.called;
             resSpy.getCall(0).args[0].data.should.be.eql([
                 {
@@ -292,102 +137,139 @@ describe('Reading meters', function() {
         });
     });
 
-    it('should return scale no more than 4 fixed-point notation', async function() {
-        const house = {
-            toJSON: () => ({
-                building: {
-                    building: 'building',
-                    unit: 'unit',
-                    location: 'location',
-                },
-                roomNumber: 'roomNumber',
-                prices: [
-                    {
-                        price: 1000,
-                    },
-                ],
-                rooms: [
-                    {
-                        id: 'roomId',
-                        name: 'roomName',
-                        contracts: [{
-                            userId: 222,
-                            user: {
-                                name: 'username',
-                            },
-                            from: 200000,
-                            to: 300000,
-                        }],
-                        devices: [
-                            {
-                                deviceId: 123,
-                            }
-                        ],
-                    },
-                ],
-            }),
-        };
-        const req = {
-            params: {
-                projectId: 100,
-            },
-            query: {
-                houseFormat: 'SHARE',
-                startDate: 100000,
-                endDate: 200000,
-            },
-
-        };
-        global.MySQL = {
-            Houses: {
-                async findAll() {
-                    return [house];
-                },
-            },
-            DeviceHeartbeats: {
-                async findAll() {
-                    return [{toJSON: () => ({deviceId: 123, startScale: 1.0000000000001, endScale: 2, startDate: 100000, endDate: 150000})}];
-                },
-            },
-            Sequelize: {
-                fn: () => {},
-                col: () => {},
-            }
-        };
-        const resSpy = spy();
-
-        await get(req, {send: resSpy}).then(() => {
-            resSpy.should.have.been.called;
-            resSpy.getCall(0).args[0].data.should.be.eql([
+    it('should return scale no more than 4 fixed-point notation',
+        async function() {
+            const houses = [
                 {
-                    location: 'location',
-                    roomId: 'roomId',
-                    roomName: 'roomName',
-                    roomNumber: 'roomNumber',
-                    unit: 'unit',
-                    building: 'building',
-                    details: [
-                        {
-                            amount: 1 * 1000,
-                            price: 1000,
-                            device: {
-                                deviceId: 123,
+                    toJSON: () => ({
+                        ...houseFields(),
+                        rooms: standardRooms(),
+                    }),
+                }];
+            const heartbeats = [
+                standardHeartbeat({
+                    startScale: 1.0000000000001,
+                    endScale: 3,
+                    startDate: 100000,
+                    endDate: 150000,
+                })];
+            global.MySQL = stubSequelizeModel(houses, heartbeats);
+            const resSpy = spy();
+
+            await get(standardRequest(), {send: resSpy}).then(() => {
+                resSpy.should.have.been.called;
+                resSpy.getCall(0).args[0].data.should.be.eql([
+                    {
+                        ...expectedFields,
+                        details: [
+                            {
+                                amount: 2 * 1000,
+                                price: 1000,
+                                device: {
+                                    deviceId: 123,
+                                },
+                                endDate: timeAlign(200000),
+                                startDate: timeAlign(100000),
+                                endScale: '3.0000',
+                                startScale: '1.0000',
+                                usage: '2.0000',
+                                userId: 222,
+                                userName: 'username',
                             },
-                            endDate: timeAlign(200000),
-                            startDate: timeAlign(100000),
-                            endScale: '2.0000',
-                            startScale: '1.0000',
-                            usage: '1.0000',
-                            userId: 222,
-                            userName: 'username',
-                        },
-                    ],
-                }]);
+                        ],
+                    }]);
+            });
         });
-    });
 
 });
 
 const timeAlign = time => moment.unix(time).
     startOf('days').
     unix();
+
+const stubSequelizeModel = (houses, heartbeats) => ({
+    Houses: {
+        async findAll() {
+            return houses;
+        },
+    },
+    DeviceHeartbeats: {
+        async findAll() {
+            return heartbeats;
+        },
+    },
+    Sequelize: {
+        fn: () => {
+        },
+        col: () => {
+        },
+    },
+});
+const defaultPrices = [
+    {
+        price: 1000,
+    },
+];
+const houseFields = (prices = defaultPrices) => ({
+    building: {
+        building: 'building',
+        unit: 'unit',
+        location: 'location',
+    },
+    roomNumber: 'roomNumber',
+    prices,
+});
+
+const expectedFields = {
+    location: 'location',
+    roomId: 'roomId',
+    roomName: 'roomName',
+    roomNumber: 'roomNumber',
+    unit: 'unit',
+    building: 'building',
+};
+
+const standardRequest = (extra = {}) => ({
+    params: {
+        projectId: 100,
+    },
+    query: {
+        houseFormat: 'SHARE',
+        startDate: 100000,
+        endDate: 200000,
+        ...extra,
+    },
+
+});
+
+const standardRooms = () => [
+    {
+        id: 'roomId',
+        name: 'roomName',
+        contracts: [
+            {
+                userId: 222,
+                user: {
+                    name: 'username',
+                },
+                from: 200000,
+                to: 300000,
+            }],
+        devices: [
+            {
+                deviceId: 123,
+            },
+        ],
+    },
+];
+
+const standardHeartbeat = (extra = {}) => ({
+    toJSON: () => ({
+        deviceId: 123,
+        startScale: 100,
+        endScale: 200,
+        startDate: 100,
+        endDate: 200,
+        ...extra,
+    }),
+});
