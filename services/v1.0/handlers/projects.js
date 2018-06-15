1ca0f4a9bc793921c37f09d566ab384f46ed1700
Louis Suo <louis.suo@myob.com>
2018-06-15 13:59:24 +1000

    add create project api

---
+++ services/v1.0/handlers/projects.js
@@ -0,0 +1,68 @@
+'use strict'
+require('include-node')
+const fp = require('lodash/fp')
+const {assignNewId} = Include('/services/v1.0/common')
+
+module.exports = {
+  post: async (req, res) => {
+    const body = req.body
+    const Projects = MySQL.Projects
+    const Auth = MySQL.Auth
+    const FundChannels = MySQL.FundChannels
+
+    if (fp.isEmpty(body.name)) {
+      return res.send(422, ErrorCode.ack(ErrorCode.PARAMETERERROR,
+        {error: 'please provide name for creating new project'}))
+    }
+
+    if (fp.isEmpty(body.admin)) {
+      return res.send(422, ErrorCode.ack(ErrorCode.PARAMETERERROR,
+        {error: 'please provide admin user name for creating new project'}))
+    }
+
+    const projectInfo = assignNewId(fp.omit('projectId')(body))
+
+    const guardFields = fp.pick(
+      ['name', 'projectId', 'logoUrl', 'address', 'description', 'telephone'])(
+      projectInfo)
+
+    const adminUser = t => Auth.create({
+      projectId: projectInfo.id,
+      username: body.admin,
+      password: 'e10adc3949ba59abbe56e057f20f883e',
+      level: 'ADMIN',
+    }, {transaction: t})
+
+    const allPayments = t => FundChannels.bulkCreate(fp.concat(
+      offlinePayments(projectInfo.id), onlinePayments(projectInfo.if)),
+    {transaction: t})
+    return MySQL.Sequelize.transaction(t =>
+      Promise.all(
+        fp.concat(Projects.create(guardFields, {transaction: t}), adminUser(t),
+          allPayments(t)))).
+      then(() => res.send(200,
+        ErrorCode.ack(ErrorCode.OK, {id: projectInfo.id}))).
+      catch(err => res.send(500,
+        ErrorCode.ack(ErrorCode.DATABASEEXEC, {error: err.message})))
+  },
+}
+
+const offlinePayments = projectId => fp.map(fp.defaults(
+  {flow: 'receive', projectId, category: 'offline', status: 'PASSED'}))(
+  [
+    {tag: 'cash', name: '现金'},
+    {tag: 'card', name: '银行转账'},
+    {tag: 'pos', name: 'POS刷卡'},
+    {tag: 'other', name: '其他'},
+    {tag: 'check', name: '支票'},
+    {tag: 'billpay', name: '账扣'},
+    {tag: 'reversal', name: '冲正'},
+    {tag: 'wechat', name: '微信转账'},
+  ])
+const onlinePayments = projectId => fp.map(fp.defaults(
+  {flow: 'receive', projectId, category: 'online', status: 'PASSED'}))(
+  [
+    {tag: 'alipay', name: '支付宝'},
+    {tag: 'wx', name: '微信'},
+  ])
+
---
+++ test/services/projects.spec.js
@@ -0,0 +1,81 @@
+'use strict';
+
+const {get, put} = require('../../services/v1.0/handlers/projects/:projectId');
+require('include-node');
+const {spy} = require('sinon');
+
+describe('Project', function () {
+  before(() => {
+    global.Typedef = Include('/libs/typedef');
+  });
+  it('should return project info by pid', async function () {
+    const projectInfo = {
+      dataValues: {
+        name: 'myName'
+      }
+    };
+    const req = {
+      params: {
+        projectId: 100
+      }
+    };
+    global.MySQL = {
+      Projects: {
+        async findOne() {
+          return projectInfo;
+        }
+      }
+    };
+
+    const resSpy = spy();
+
+    await get(req, {send: resSpy}).then(() => {
+      const response = resSpy.getCall(0).args[0];
+      response.should.be.eql(projectInfo.dataValues);
+    });
+  });
+
+  it('should allow to update project partially', async function () {
+    const req = {
+      params: {
+        projectId: 100
+      },
+      body: {
+        id: 312,
+        name: 'myName'
+      }
+    };
+    global.MySQL = {
+      Projects: {
+        async update() {
+          return {id: req.body.id};
+        }
+      }
+    };
+
+    const resSpy = spy();
+
+    await put(req, {send: resSpy}).then(() => {
+      resSpy.getCall(0).args[0].should.be.eql(200);
+      resSpy.getCall(0).args[1].result.should.be.eql({id: 312});
+    });
+  });
+
+  it('should give 400 if no project db id is provided', async function () {
+    const req = {
+      params: {
+        projectId: 100
+      },
+      body: {
+        name: 'myName'
+      }
+    };
+
+    const resSpy = spy();
+
+    await put(req, {send: resSpy}).then(() => {
+      resSpy.getCall(0).args[0].should.be.eql(400);
+      resSpy.getCall(0).args[1].result.should.be.eql({error: 'please provide db id of this project'});
+    });
+  });
+});
