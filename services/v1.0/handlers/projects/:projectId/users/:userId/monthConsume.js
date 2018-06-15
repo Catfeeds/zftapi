034625d8feeb0d2027f1f7dccb01ab4b327587ad
Louis Suo <louis.suo@myob.com>
2018-06-15 21:10:49 +1000

    fix month cosumption api

--- services/v1.0/handlers/projects/:projectId/users/:userId/monthConsume.js
+++ services/v1.0/handlers/projects/:projectId/users/:userId/monthConsume.js
@@ -1,78 +1,61 @@
-'use strict'
-const moment = require('moment')
-
-/**
- * Operations on /fundChannels/{fundChannelId}
- */
+'use strict';
+const moment = require('moment');
 
 module.exports = {
-  /**
-     * summary: topup
-     * description:
+  get: async (req, res) => {
+    const projectId = req.params.projectId;
+    const userId = req.params.userId;
 
-     * parameters: hid
-     * produces: application/json
-     * responses: 200, 400
-     */
-  get: (req, res)=>{
-    /**
-         * Get the data for response 200
-         * For response `default` status 200 is used.
-         */
-    (async()=>{
-      const projectId = req.params.projectId
-      const userId = req.params.userId
+    if (!Util.ParameterCheck(req.query, ['month'])) {
+      return res.send(422,
+        ErrorCode.ack(ErrorCode.PARAMETERMISSED, 'please provide month'));
+    }
 
-      if(!Util.ParameterCheck(req.query, ['month'])){
-        return res.send(422, ErrorCode.ack(ErrorCode.PARAMETERMISSED, 'please provide month'))
-      }
+    const month = req.query.month;
+    const startDate = moment(month, 'YYYYMM').startOf('days');
+    const endDate = moment(month, 'YYYYMM').endOf('days');
 
-      const month = req.query.month
-      const startDate = moment(month, 'YYYYMM').startOf('days')
-      const endDate = moment(month, 'YYYYMM').endOf('days')
+    if (!startDate.isValid() || !endDate.isValid()) {
+      return res.send(400,
+        ErrorCode.ack(ErrorCode.PARAMETERERROR, {month: month}));
+    }
 
-      if(!startDate.isValid() || !endDate.isValid() ){
-        return res.send(400, ErrorCode.ack(ErrorCode.PARAMETERERROR, {month: month}))
-      }
+    //
+    const contract = await  MySQL.Contracts.findOne({
+      where: {
+        userId,
+        status: Typedef.ContractStatus.ONGOING,
+      },
+      attributes: ['id'],
+    });
 
-      //
-      const contract = await  MySQL.Contracts.findOne({
-        where: {
-          userId: userId
-          , status: Typedef.ContractStatus.ONGOING
-        }
-        ,attributes: ['id']
-      })
-      if(!contract){
-        return res.send(404, ErrorCode.ack(ErrorCode.CONTRACTNOTEXISTS))
-      }
+    if (!contract) {
+      return res.send(404, ErrorCode.ack(ErrorCode.CONTRACTNOTEXISTS));
+    }
 
-      const options =  {
-        where:{
-          contractId: contract.id,
-          paymentDay:{
-            $gte: startDate.unix()
-            , $lte: endDate.unix()
-          }
-        }
-      }
-      Promise.all([
-        MySQL.DevicePrePaid.sum('amount', options)
-        , MySQL.DailyPrePaid.sum('amount', options)
-      ]).then(
-        sumResult => {
-          const consume = sumResult[0] || 0 + sumResult[1] || 0
-          res.send({
-            month: month,
-            consume: Math.abs(consume)
-          })
-        }
-        , err => {
-          log.error(err, projectId, userId, month)
-          res.send(500, ErrorCode.ack())
-        }
-      )
-
-    })()
+    const options = {
+      where: {
+        contractId: contract.id,
+        paymentDay: {
+          $gte: startDate.unix(),
+          $lte: endDate.unix(),
+        },
+      },
+    };
+    Promise.all([
+      MySQL.DevicePrePaid.sum('amount', options)
+      , MySQL.DailyPrePaid.sum('amount', options),
+    ]).then(
+      ([device, daily]) => {
+        const consume = (device || 0) + (daily || 0);
+        res.send({
+          month,
+          consume,
+        });
+      },
+    ).catch(err => {
+      log.error('', err, projectId, userId, month);
+      res.send(500, ErrorCode.ack());
+    });
   },
-}
+};
