ee6b7af4219e75ded24bf46ead2275633001b9c1
Louis Suo <louis.suo@myob.com>
2018-06-26 01:31:12 +1000

    fix lint

--- services/v1.0/handlers/boss/fundChannels.js
+++ services/v1.0/handlers/boss/fundChannels.js
@@ -1,9 +1,9 @@
-const sequelize = require('sequelize')
+// const sequelize = require('sequelize')
 module.exports = {
   get: async (req, res) => {
     const send = res.send.bind(res)
     MySQL.Exec(
-      "select count(*) as value, f.name from billpayment as b join fundChannels as f where b.`fundChannelId`=f.id group by f.name")
+      'select count(*) as value, f.name from billpayment as b join fundChannels as f where b.`fundChannelId`=f.id group by f.name')
       .then(send)
   }
 }
