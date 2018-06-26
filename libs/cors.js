e55bac94d31f1379924c40d7bbe5c28391acb721
Jichao Ouyang <oyanglulu@gmail.com>
2018-06-26 18:46:06 +1000

    origin already has protocal

--- libs/cors.js
+++ libs/cors.js
@@ -6,7 +6,8 @@ const {URL} = require('url');
 const frontendOrigin = 'https://saas.51dianxiaoge.com'
 exports.allowOrigin = (domains = frontendOrigin) => (req) => {
   const host = fp.getOr(frontendOrigin)('headers.origin')(req);
-  const origin = new URL(`https://${host}` || frontendOrigin).origin;
+  const origin = new URL(host || frontendOrigin).origin;
   const allDomains = fp.split(',')(domains);
+  console.log(allDomains, origin, req)
   return fp.includes(origin)(allDomains) ? origin : fp.head(allDomains);
 };
--- test/libs/cors.spec.js
+++ test/libs/cors.spec.js
@@ -7,7 +7,7 @@ const {
 describe('CORS', () => {
   it('should allow origin if they are included in config', () => {
     allowOrigin('https://abc.com,https://cde.com')(
-      {headers: {origin: 'cde.com'}}).
+      {headers: {origin: 'https://cde.com'}}).
       should.
       be.
       equal('https://cde.com')
@@ -15,14 +15,14 @@ describe('CORS', () => {
 
   it('should allow the first origin if they are not included in config', () => {
     allowOrigin('https://abc.com,https://cde.com')(
-      {headers: {origin: 'nonexists.com'}}).
+      {headers: {origin: 'http://nonexists.com'}}).
       should.
       be.
       equal('https://abc.com')
   })
 
   it('should allow prod front end url as default', () => {
-    allowOrigin()({headers: {origin: 'nonexists.com'}}).
+    allowOrigin()({headers: {origin: 'http://nonexists.com'}}).
       should.
       be.
       equal('https://saas.51dianxiaoge.com')
