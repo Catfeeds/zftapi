af058b5b6e3ecb1cbb5f0d49b1dbe3aa880328eb
Jichao Ouyang <oyanglulu@gmail.com>
2018-06-26 18:54:43 +1000

    rm log

--- libs/cors.js
+++ libs/cors.js
@@ -8,6 +8,5 @@ exports.allowOrigin = (domains = frontendOrigin) => (req) => {
   const host = fp.getOr(frontendOrigin)('headers.origin')(req);
   const origin = new URL(host || frontendOrigin).origin;
   const allDomains = fp.split(',')(domains);
-  console.log(allDomains, origin, req)
   return fp.includes(origin)(allDomains) ? origin : fp.head(allDomains);
 };
