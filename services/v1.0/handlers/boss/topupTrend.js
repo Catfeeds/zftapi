680fa56cd228718af1050f85e71aab5f6ff6fa89
Jichao Ouyang <oyanglulu@gmail.com>
2018-06-29 22:31:14 +1000

    fix lint

--- services/v1.0/handlers/boss/topupTrend.js
+++ services/v1.0/handlers/boss/topupTrend.js
@@ -1,8 +1,10 @@
 module.exports = {
   get: (req, res) => {
     const send = res.send.bind(res)
+    /*eslint-disable */
     MySQL.Exec(
       `select DATE_FORMAT(f.createdAt, "%Y-%m-%d %H:00") as time, sum(f.amount) as value from flows f where f.category = 'topup' group by DATE_FORMAT(f.createdAt, "%Y-%m-%d %H:00")`)
       .then(send)
+    /*eslint-disable */
   }
 }
--- services/v1.0/handlers/reading.js
+++ services/v1.0/handlers/reading.js
@@ -5,7 +5,7 @@
 module.exports = {
   post: async (req, res) => {
     const content = JSON.stringify(req.body)
-    console.log(`nb reading got: ${content}`);
+    console.log(`nb reading got: ${content}`)
     return MySQL.NBReading.create({content}).then(state => {
       res.send(200, ErrorCode.ack(ErrorCode.OK, state))
     })
