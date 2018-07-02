36c92f31161a40c3aec1b777cbdaaf3ada36855a
Jichao Ouyang <oyanglulu@gmail.com>
2018-07-02 22:25:14 +1000

    timespan fix to this year

--- services/v1.0/handlers/boss/projectTopup.js
+++ services/v1.0/handlers/boss/projectTopup.js
@@ -3,7 +3,7 @@ module.exports = {
     const send = res.send.bind(res)
     /*eslint-disable */
     MySQL.Exec(
-      "select sum(f.amount) as value, p.name, f.projectId from flows as f, projects as p where f.category = 'topup' and f.direction='receive' and f.`projectId` = p.id group by f.projectId order by value desc limit 10")
+      "select sum(f.amount) as value, p.name, f.projectId from flows as f, projects as p where f.category = 'topup' and f.direction='receive' and f.`projectId` = p.id and f.`createdAt` between DATE_FORMAT(NOW() ,'%Y-01-01') and NOW() group by f.projectId order by value desc limit 10")
       .then(send)
     /*eslint-disable */
   }
