d2e24e75f0f33292becc0cb7cb2b9fb9e93f27a1
Louis Suo <louis.suo@myob.com>
2018-07-17 22:37:39 +1000

    format code, returns 204 in deleting houses

--- services/v1.0/handlers/projects/:projectId/communities.js
+++ services/v1.0/handlers/projects/:projectId/communities.js
@@ -1,26 +1,25 @@
 'use strict'
 const fp = require('lodash/fp')
 
-function BuildDivisionTree(districts, inDivision)
-{
+function BuildDivisionTree(districts, inDivision) {
   //
   let tree = {}
-  districts.map(district=>{
+  districts.map(district => {
     const districtId = district.id.toString()
     const parentDivision = Util.ParentDivisionId(districtId)
-    if(districtId === parentDivision){
+    if (districtId === parentDivision) {
       tree[districtId] = {
         districtId: districtId,
         name: district.title,
-        districts:{}
+        districts: {},
       }
     }
-    else{
+    else {
       let districtInfo = {
         districtId: districtId,
-        name: district.title
+        name: district.title,
       }
-      if(inDivision[districtId]){
+      if (inDivision[districtId]) {
         districtInfo.communities = inDivision[districtId]
       }
       tree[parentDivision].districts[districtId] = districtInfo
@@ -34,80 +33,77 @@ function BuildDivisionTree(districts, inDivision)
  * Operations on /communities/:communityId
  */
 module.exports = {
-  get: function getCommunity(req, res) {
+  get: async (req, res) => {
 
-    (async()=>{
-      const param = req.params
-      const query = req.query
+    const param = req.params
+    const query = req.query
 
-      if(!Util.ParameterCheck(query,
-        ['houseFormat']
-      )){
-        return res.send(422, ErrorCode.ack(ErrorCode.PARAMETERMISSED))
-      }
-      const houseFormat = query.houseFormat
-      const projectId = param.projectId
+    if (!Util.ParameterCheck(query,
+      ['houseFormat'],
+    )) {
+      return res.send(422, ErrorCode.ack(ErrorCode.PARAMETERMISSED))
+    }
+    const houseFormat = query.houseFormat
+    const projectId = param.projectId
 
+    // let sql;
+    const replacements = {
+      houseFormat: houseFormat,
+      projectId: projectId,
+    }
 
-      // let sql;
-      const replacements = {
-        houseFormat: houseFormat,
-        projectId: projectId
-      }
-
-      try {
-        const sql = `select distinct(bu.locationId),bu.id as buildingId from ${MySQL.Houses.name} as h 
+    try {
+      const sql = `select distinct(bu.locationId),bu.id as buildingId from ${MySQL.Houses.name} as h 
                     inner join buildings as bu on h.buildingId=bu.id 
                      where h.deleteAt=0 and  bu.deleteAt=0 and h.houseFormat=:houseFormat and h.projectId=:projectId`
-        const locations = await MySQL.Exec(sql, replacements)
-        let geoLocationIds = []
-        let locationBuilding = {}
-        locations.map(r=>{
-          geoLocationIds.push(r.locationId)
-          locationBuilding[r.locationId] = r.buildingId
-        })
+      const locations = await MySQL.Exec(sql, replacements)
+      let geoLocationIds = []
+      let locationBuilding = {}
+      locations.map(r => {
+        geoLocationIds.push(r.locationId)
+        locationBuilding[r.locationId] = r.buildingId
+      })
 
-        if(!locations.length){
-          return res.send([])
+      if (!locations.length) {
+        return res.send([])
+      }
+
+      const geoLocations = await MySQL.GeoLocation.findAll({
+        where: {
+          id: {$in: geoLocationIds},
+        },
+        attributes: ['id', 'divisionId', 'name'],
+      })
+      let districtIds = []
+      let inDivision = {}
+      geoLocations.map(loc => {
+        const districtId = loc.divisionId
+
+        const parentDivision = Util.ParentDivisionId(districtId.toString())
+        districtIds.push(parentDivision)
+        districtIds.push(districtId)
+        if (!inDivision[districtId]) {
+          inDivision[districtId] = []
         }
-
-        const geoLocations = await MySQL.GeoLocation.findAll({
-          where: {
-            id: {$in: geoLocationIds}
-          },
-          attributes: ['id', 'divisionId', 'name']
+        inDivision[districtId].push({
+          geoLocationId: loc.id,
+          buildingId: locationBuilding[loc.id],
+          name: loc.name,
         })
-        let districtIds = []
-        let inDivision = {}
-        geoLocations.map(loc => {
-          const districtId = loc.divisionId
+      })
+      districtIds = fp.uniq(districtIds)
 
-          const parentDivision = Util.ParentDivisionId(districtId.toString())
-          districtIds.push(parentDivision)
-          districtIds.push(districtId)
-          if (!inDivision[districtId]) {
-            inDivision[districtId] = []
-          }
-          inDivision[districtId].push({
-            geoLocationId: loc.id,
-            buildingId: locationBuilding[loc.id],
-            name: loc.name
-          })
-        })
-        districtIds = fp.uniq(districtIds)
+      const districts = await MySQL.Divisions.findAll({
+        where: {
+          id: {$in: districtIds},
+        },
+        attributes: ['id', 'title'],
+      })
 
-        const districts = await MySQL.Divisions.findAll({
-          where: {
-            id: {$in: districtIds}
-          },
-          attributes: ['id', 'title']
-        })
-
-        res.send( BuildDivisionTree(districts, inDivision) )
-      }
-      catch(e){
-        log.error(e, projectId, houseFormat)
-      }
-    })()
-  }
+      res.send(BuildDivisionTree(districts, inDivision))
+    }
+    catch (e) {
+      log.error(e, projectId, houseFormat)
+    }
+  },
 }
--- services/v1.0/handlers/projects/:projectId/houses/:houseId.js
+++ services/v1.0/handlers/projects/:projectId/houses/:houseId.js
@@ -8,19 +8,19 @@ const moment = require('moment')
 
 module.exports = {
   /**
-     * summary: get specified houses by hid
-     * description: pass hid or query parameter to get houese list
+   * summary: get specified houses by hid
+   * description: pass hid or query parameter to get houese list
 
-     * parameters: hid
-     * produces: application/json
-     * responses: 200, 400
-     */
-  get: (req, res)=>{
+   * parameters: hid
+   * produces: application/json
+   * responses: 200, 400
+   */
+  get: (req, res) => {
     /**
-         * Get the data for response 200
-         * For response `default` status 200 is used.
-         */
-    (async()=>{
+     * Get the data for response 200
+     * For response `default` status 200 is used.
+     */
+    (async () => {
       const params = req.params
 
       const id = params.houseId
@@ -29,32 +29,45 @@ module.exports = {
       const houseIns = await MySQL.Houses.findOne({
         where: {
           id: id,
-          projectId: projectId
+          projectId: projectId,
         },
         subQuery: false,
         include: [
           {
             model: MySQL.Building, as: 'building'
-            , include:[{
-              model: MySQL.GeoLocation, as: 'location'
-            }]
-            , attributes: ['group', 'building', 'unit']
+            , include: [
+              {
+                model: MySQL.GeoLocation, as: 'location',
+              }]
+            , attributes: ['group', 'building', 'unit'],
           },
-          {model: MySQL.Layouts, as: 'layouts', attributes: ['id', 'name','bedRoom', 'livingRoom', 'bathRoom', 'orientation', 'roomArea', 'remark']},
+          {
+            model: MySQL.Layouts,
+            as: 'layouts',
+            attributes: [
+              'id',
+              'name',
+              'bedRoom',
+              'livingRoom',
+              'bathRoom',
+              'orientation',
+              'roomArea',
+              'remark'],
+          },
           await common.includeRoom(),
           {
             model: MySQL.HouseDevicePrice,
             as: 'prices',
             required: false,
-            where:{
-              endDate: 0
+            where: {
+              endDate: 0,
             },
-            attributes:['type', 'price']
+            attributes: ['type', 'price'],
           },
-          common.includeHouseDevices(true)
-        ]
+          common.includeHouseDevices(true),
+        ],
       })
-      if(!houseIns){
+      if (!houseIns) {
         return res.send(404, ErrorCode.ack(ErrorCode.REQUESTUNMATCH))
       }
 
@@ -74,139 +87,137 @@ module.exports = {
         layout: house.layouts,
         config: house.config,
         rooms: common.translateRooms(house.layouts)(house.rooms),
-        devices: common.translateDevices(house.devices)
+        devices: common.translateDevices(house.devices),
       })
     })()
   },
   /**
-     * summary: delete house
-     * description: save house information
+   * summary: delete house
+   * description: save house information
 
-     * parameters: hid
-     * produces: application/json
-     * responses: 200, 400, 405, 410
-     */
-  delete: function deleteHouse(req, res) {
-    /**
-         * Get the data for response 200
-         * For response `default` status 200 is used.
-         */
-    (async()=>{
-      const houseId = req.params.houseId
-      const projectId = req.params.projectId
+   * parameters: hid
+   * produces: application/json
+   * responses: 200, 400, 405, 410
+   */
+  delete: async (req, res) => {
+    const houseId = req.params.houseId
+    const projectId = req.params.projectId
 
-      const isExists = await MySQL.Houses.count({
-        where:{
-          id: houseId,
-          deleteAt: 0,
-          projectId: projectId,
-          //TODO: can we delete closed houses?
-          status: Typedef.HouseStatus.OPEN,
-        }
-      })
-      if(!isExists){
-        return res.send(412, ErrorCode.ack(ErrorCode.STATUSUNMATCH, {message: `please check the status of house ${houseId}.`}))
-      }
+    const isExists = await MySQL.Houses.count({
+      where: {
+        id: houseId,
+        deleteAt: 0,
+        projectId: projectId,
+        //TODO: can we delete closed houses?
+        status: Typedef.HouseStatus.OPEN,
+      },
+    })
+    if (!isExists) {
+      return res.send(412, ErrorCode.ack(ErrorCode.STATUSUNMATCH,
+        {message: `please check the status of house ${houseId}.`}))
+    }
 
-      const now = moment().unix()
-      const rooms = await MySQL.Rooms.findAll({
-        where:{
-          houseId: houseId
-        },
-        include:[
-          {
-            model: MySQL.Contracts,
-            as: 'contracts',
-            where:{
-              to:{$or:[
+    const now = moment().unix()
+    const rooms = await MySQL.Rooms.findAll({
+      where: {
+        houseId: houseId,
+      },
+      include: [
+        {
+          model: MySQL.Contracts,
+          as: 'contracts',
+          where: {
+            to: {
+              $or: [
                 {$eq: 0},
-                {$gte: now}
-              ]}
+                {$gte: now},
+              ],
             },
-            required: false
-          }
-        ]
-      })
-
-      const isRoomInUse = fp.compact(fp.map(room=>{
-        return common.roomLeasingStatus(room.contracts) !== Typedef.OperationStatus.IDLE ? room.id : null
-      })(rooms))
-
-      if(isRoomInUse.length){
-        return res.send(400, ErrorCode.ack(ErrorCode.CONTRACTWORKING))
-      }
-
-      let t
-      try {
-        t = await MySQL.Sequelize.transaction({autocommit: false})
-
-        const now = moment()
-        await MySQL.Houses.update(
-          {
-            deleteAt: now.unix(),
-            status: Typedef.OperationStatus.DELETED
           },
-          {
-            where: {
-              id: houseId,
-              projectId: projectId
-            },
-            transaction: t
-          }
-        )
-        await MySQL.Layouts.update(
-          {
-            deleteAt: now.unix(),
-          },
-          {
-            where:{
-              sourceId: houseId
-            },
-            transaction: t
-          }
-        )
-        await MySQL.Rooms.destroy(
-          {
-            where:{
-              houseId: houseId
-            },
-            transaction: t
-          }
-        )
-        await MySQL.HouseDevices.destroy(
-          {
-            where:{
-              sourceId: houseId,
-              endDate: 0
-            },
-            transaction: t
-          }
-        )
+          required: false,
+        },
+      ],
+    })
 
-        await t.commit()
-        res.send(201)
-      }
-      catch(e){
-        log.error(e, houseId)
-        await t.rollback()
-        res.send(500, ErrorCode.ack(ErrorCode.DATABASEEXEC))
-      }
-    })()
+    const isRoomInUse = fp.compact(fp.map(room => {
+      return common.roomLeasingStatus(room.contracts) !==
+      Typedef.OperationStatus.IDLE ? room.id : null
+    })(rooms))
+
+    if (isRoomInUse.length) {
+      return res.send(400, ErrorCode.ack(ErrorCode.CONTRACTWORKING))
+    }
+
+    let t
+    try {
+      t = await MySQL.Sequelize.transaction({autocommit: false})
+
+      const now = moment()
+      await MySQL.Houses.update(
+        {
+          deleteAt: now.unix(),
+          status: Typedef.OperationStatus.DELETED,
+        },
+        {
+          where: {
+            id: houseId,
+            projectId: projectId,
+          },
+          transaction: t,
+        },
+      )
+      await MySQL.Layouts.update(
+        {
+          deleteAt: now.unix(),
+        },
+        {
+          where: {
+            sourceId: houseId,
+          },
+          transaction: t,
+        },
+      )
+      await MySQL.Rooms.destroy(
+        {
+          where: {
+            houseId: houseId,
+          },
+          transaction: t,
+        },
+      )
+      await MySQL.HouseDevices.destroy(
+        {
+          where: {
+            sourceId: houseId,
+            endDate: 0,
+          },
+          transaction: t,
+        },
+      )
+
+      await t.commit()
+      res.send(204)
+    }
+    catch (e) {
+      log.error(e, houseId)
+      await t.rollback()
+      res.send(500, ErrorCode.ack(ErrorCode.DATABASEEXEC))
+    }
   },
   /**
-     * summary: update house
-     * description: save house information
+   * summary: update house
+   * description: save house information
 
-     * parameters: hid, body
-     * produces: application/json
-     * responses: 200, 400
-     */
+   * parameters: hid, body
+   * produces: application/json
+   * responses: 200, 400
+   */
   put: function updateHouse(req, res) {
     /**
-         * Get the data for response 200
-         * For response `default` status 200 is used.
-         */
-    (async()=>{
+     * Get the data for response 200
+     * For response `default` status 200 is used.
+     */
+    (async () => {
       const body = req.body
       const params = req.params
 
@@ -214,31 +225,33 @@ module.exports = {
       const houseId = params.houseId
 
       const houseIns = await MySQL.Houses.findOne({
-        where:{
+        where: {
           id: houseId,
-          projectId: projectId
-        }
+          projectId: projectId,
+        },
       })
 
-      if(!houseIns){
+      if (!houseIns) {
         return res.send(404, ErrorCode.ack(ErrorCode.REQUESTUNMATCH))
       }
 
       const putBody = fp.pick(body
-        , ['location', 'code', 'group', 'building', 'unit',
+        , [
+          'location', 'code', 'group', 'building', 'unit',
           'roomNumber', 'totalFloor', 'currentFloor',
-          'config', 'houseKeeper', 'layout']
+          'config', 'houseKeeper', 'layout'],
       )
 
-      try{
+      try {
         const t = await MySQL.Sequelize.transaction()
 
-        if(body.location) {
-          const newLocation = await common.AsyncUpsertGeoLocation(body.location, t)
+        if (body.location) {
+          const newLocation = await common.AsyncUpsertGeoLocation(body.location,
+            t)
           body.location = MySQL.Plain(newLocation[0])
         }
 
-        if(!fp.isEmpty(putBody)) {
+        if (!fp.isEmpty(putBody)) {
           await MySQL.Houses.update(
             putBody,
             {
@@ -246,8 +259,8 @@ module.exports = {
                 id: houseId,
                 projectId: projectId,
               },
-              transaction: t
-            }
+              transaction: t,
+            },
           )
 
           await MySQL.Building.update(
@@ -255,10 +268,10 @@ module.exports = {
             {
               where: {
                 id: houseIns.buildingId,
-                projectId: projectId
+                projectId: projectId,
               },
-              transaction: t
-            }
+              transaction: t,
+            },
           )
 
           await MySQL.Layouts.update(
@@ -266,20 +279,20 @@ module.exports = {
             {
               where: {
                 id: body.layout.id,
-                sourceId: houseId
+                sourceId: houseId,
               },
-              transaction: t
-            }
+              transaction: t,
+            },
           )
         }
 
         await t.commit()
         res.send(204)
       }
-      catch(e){
+      catch (e) {
         log.error(ErrorCode.ack(e.message), params, body)
         res.send(500, ErrorCode.ack(ErrorCode.DATABASEEXEC))
       }
     })()
-  }
+  },
 }
