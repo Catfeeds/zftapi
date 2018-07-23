76ec020ecc9c0cc6cfe5ac6ae97b162b25abab92
Jichao Ouyang <oyanglulu@gmail.com>
2018-07-23 22:29:18 +1000

    add put endpoints for pay and fund channel

---
+++ services/v1.0/handlers/boss/fundChannels/:channelId.js
@@ -0,0 +1,11 @@
+module.exports = {
+  put: async function (req, res) {
+    const body = req.body
+    let updated = await MySQL.FundChannels.update(body, {
+      where: {
+        id: req.params.channelId
+      }
+    })
+    res.send(200, ErrorCode.ack(ErrorCode.OK, {id: updated.id}))
+  }
+}
