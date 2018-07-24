903dff032446301ea367eeb10f57b6c68ea7b2bf
Jichao Ouyang <oyanglulu@gmail.com>
2018-07-24 21:41:29 +1000

    temp comment out join balance

--- services/v1.0/handlers/boss/payChannels.js
+++ services/v1.0/handlers/boss/payChannels.js
@@ -1,6 +1,7 @@
 module.exports = {
   get: async (req, res) => {
     let payChannel = await MySQL.PayChannels.findAll({
+      order:[['createdAt', 'DESC']],
       include:[{
         model: MySQL.FundChannels,
         as: 'fundChannel',
--- services/v1.0/handlers/boss/withDraw.js
+++ services/v1.0/handlers/boss/withDraw.js
@@ -1,12 +1,21 @@
 module.exports = {
   get: async (req, res) => {
+    const fn = MySQL.Sequelize.fn
     let withDraw = await MySQL.WithDraw.findAll({
+      // attributes: [
+      //   [fn('sum', MySQL.Sequelize.col('channel->project->fundChannelFlows.amount')), 'sum'],
+      // ],
+      order:[['createdAt', 'DESC']],
       include:[{
         model: MySQL.FundChannels,
         as: 'channel',
         include:[{
           model:MySQL.Projects,
-          as: 'project'
+          as: 'project',
+          // include:[{
+          //   model: MySQL.FundChannelFlows,
+          //   as: 'fundChannelFlows'
+          // }]
         }]
       }]
     })
