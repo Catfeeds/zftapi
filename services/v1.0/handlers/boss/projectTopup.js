b8b83d132f1b3c32ee5e31a8a8ba712151fc43c5
Jichao Ouyang <oyanglulu@gmail.com>
2018-08-01 20:53:37 +1000

    fixing issue https://github.com/cloudenergy/boss/issues/8

--- services/v1.0/handlers/boss/projectTopup.js
+++ services/v1.0/handlers/boss/projectTopup.js
@@ -3,7 +3,7 @@ module.exports = {
     const send = res.send.bind(res)
     /*eslint-disable */
     MySQL.Exec(
-      "select sum(f.amount) as value, p.name, f.projectId from flows as f, projects as p where f.category = 'topup' and f.direction='receive' and f.`projectId` = p.id and f.`createdAt` between DATE_FORMAT(NOW() ,'%Y-01-01') and NOW() group by f.projectId order by value desc limit 10")
+      "select sum(f.amount) as value, p.name, f.projectId from fundChannelFlows as f, fundChannels as fc, projects as p where f.`fundChannelId`=fc.id and fc.flow='receive' and fc.`category`='online' and f.`projectId` = p.id and f.`createdAt` between DATE_FORMAT(NOW() ,'%Y-01-01') and NOW() group by f.projectId order by value desc limit 10")
       .then(send)
     /*eslint-disable */
   }
