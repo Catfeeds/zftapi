1db18b06b7d25f4b054589388850ed5344251d44
Jichao Ouyang <oyanglulu@gmail.com>
2018-08-08 11:04:59 +0800

    group result

--- services/v1.0/handlers/boss/topup.js
+++ services/v1.0/handlers/boss/topup.js
@@ -3,7 +3,7 @@ module.exports = {
     const send = res.send.bind(res)
     /*eslint-disable */
     MySQL.Exec(
-      `select t.amount, t.fee, t.balance, f.name, u.name, t.orderNo, t.createdAt, b.remark from billpayment b, topup t, users u, auth a , fundChannels f where t.operator = a.id and u.id = t.userId and f.id = t.fundChannelId order by t.createdAt desc`)
+      `select t.amount, t.fee, t.balance, f.name, u.name, t.orderNo, t.createdAt, b.remark from billpayment b, topup t, users u, auth a , fundChannels f where t.operator = a.id and u.id = t.userId and f.id = t.fundChannelId group by t.orderNo order by t.createdAt desc`)
       .then(send)
     /*eslint-disable */
   }
