f9e03553830c9bb4c8d5c98859b54c0165417628
Jichao Ouyang <oyanglulu@gmail.com>
2018-08-09 08:36:06 +0800

    distinguish channel name and user name

--- services/v1.0/handlers/boss/topup.js
+++ services/v1.0/handlers/boss/topup.js
@@ -10,10 +10,11 @@ module.exports = {
     }
     /*eslint-disable */
     MySQL.Exec(
-      `select t.amount, t.fee, t.balance, f.name, u.name, t.orderNo, t.createdAt, b.remark
+      `select t.amount, t.fee, t.balance, f.name as channel, u.name, t.orderNo, t.createdAt, b.remark
        from billpayment b, topup t, users u, auth a , fundChannels f
        where t.operator = a.id and u.id = t.userId and f.id = t.fundChannelId
          and t.createdAt <= :to and t.createdAt >= :from
+       group by t.orderNo
        order by t.createdAt desc`, {
          from, to
        })
