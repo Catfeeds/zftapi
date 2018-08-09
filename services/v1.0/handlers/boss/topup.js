2c884daa09fba1fa633155cc149442d9fe232935
Jichao Ouyang <oyanglulu@gmail.com>
2018-08-09 08:42:04 +0800

    add payment status

--- services/v1.0/handlers/boss/topup.js
+++ services/v1.0/handlers/boss/topup.js
@@ -10,7 +10,7 @@ module.exports = {
     }
     /*eslint-disable */
     MySQL.Exec(
-      `select t.amount, t.fee, t.balance, f.name as channel, u.name, t.orderNo, t.createdAt, b.remark
+      `select t.amount, t.fee, t.balance, f.name as channel, u.name, t.orderNo, t.createdAt, b.remark, b.status
        from billpayment b, topup t, users u, auth a , fundChannels f
        where t.operator = a.id and u.id = t.userId and f.id = t.fundChannelId
          and t.createdAt <= :to and t.createdAt >= :from
